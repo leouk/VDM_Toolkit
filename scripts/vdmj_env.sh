f3adf8079dbac418272154cd73de5b9f8e451b5c
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2021-10-01 14:58:37 +0100

    minor

--- scripts/vdmj_env.sh
+++ scripts/vdmj_env.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 #
-# Rebuild and installs VDMJ with new jars
+# Set VDMJ environment variables and installs VDMJ with new jars
 #
 
-# Assuming:
-# 0. symbolic link vdmj_build into /usr/local/bin first
-# 1. $VDMJ_HOME and $VDMJ_VERSION is set
+if [ $# -ne 2 ]
+then
+    echo "vdmj_env.sh <VDMJ GitHub HOME> <VDMJ suite version X.Y.Z>"
+    exit 1
+fi
+
+VDMJ_HOME=$1
+VDMJ_VERSION=$2-SNAPSHOT
+VDMJ_TARGET=/usr/local/lib
 
 if [ ! -e $VDMJ_HOME ]
 then
@@ -18,41 +24,17 @@ fi
 if [ -z "$VDMJ_VERSION" ]
 then
 	# should be VDMJ_VERSION=X.Y.Z-SNAPSHOT
-    echo "Variable VDMJ_VERSION not set; should be X.Y.Z-SNAPSHOT"
+    echo "Variable VDMJ_VERSION not set; should be X.Y.Z"
     exit 1
 fi
 
 if [ ! -e $VDMJ_TARGET ]
 then
     echo "Setting VDMJ linking target to /usr/local/lib"
-    exit 1
+    VDMJ_TARGET=/usr/local/lib
 fi
 
-echo "Pull latest version of VDMJ from $VDMJ_HOME"
-cd $VDMJ_HOME
-git pull
 
-echo "Building VDMJ $VDMJ_VERSION from $VDMJ_HOME"
-
-function latest()
-{
-    ls -t $1 | head -1
-}
-
-HERE=$(pwd)
-VERSION=$VDMJ_VERSION
-VDMJ_TARGET=/usr/local/lib
-
-# You must make sure you are at VDMJ_HOME first (e.g. top-level VDMJ repo directory)
-mvn clean install
-
-echo "Linking VDMJ $VDMJ_VERSION to $VDMJ_TARGET"
-ln -snf $(latest "${HERE}/Annotations/target/annotations-${VERSION}-??????.jar") ${VDMJ_TARGET}/annotations-${VERSION}.jar
-ln -snf $(latest "${HERE}/Annotations2/target/annotations2-${VERSION}-??????.jar") ${VDMJ_TARGET}/annotations2-${VERSION}.jar
-ln -snf $(latest "${HERE}/cmd-plugins/target/cmd-plugins-${VERSION}-??????.jar") ${VDMJ_TARGET}/cmd-plugins-${VERSION}.jar
-ln -snf $(latest "${HERE}/vdmj/target/vdmj-${VERSION}-??????.jar") ${VDMJ_TARGET}/vdmj-${VERSION}.jar
-
-vdmj -v
 
 exit 0
 
