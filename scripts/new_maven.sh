11963894105ae7a069464f3d7c37719935b9ec0f
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2021-10-03 11:59:35 +0100

    maven stuff + vdmj properties

---
+++ scripts/new_maven.sh
@@ -0,0 +1,3 @@
+mvn archetype:generate -DgroupId=MyPackage -DartifactId=MyProject -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+
+mvn archetype:generate -DgroupId=com.github.vdm_toolkit -DartifactId=ConwayGL -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
