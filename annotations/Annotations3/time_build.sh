028cb03649663280405f322092b217cb0c1a4205
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2021-10-04 11:10:13 +0100

    update @Time annotation POM; normalize wrt VDMJ

--- annotations/Annotations3/time_build.sh
+++ annotations/Annotations3/time_build.sh
@@ -9,11 +9,13 @@ function latest()
 }
 
 HERE=$(pwd)
-VERSION=4.4.0-SNAPSHOT
+VERSION=4.4.4-SNAPSHOT
 VDMJ_TARGET=/usr/local/lib
 
 mvn clean install
 ln -snf $(latest "${HERE}/target/annotations3-${VERSION}-??????.jar") ${VDMJ_TARGET}/annotations3-${VERSION}.jar
 
-vdmj -i MinimalTime.vdmsl
-
+echo "Play with time - inner calls"
+vdmj -i -e "f(3,4)" MinimalTime.vdmsl
+echo "Play with time - just time summary"
+vdmj -i -e "g(3,4)" MinimalTime.vdmsl
