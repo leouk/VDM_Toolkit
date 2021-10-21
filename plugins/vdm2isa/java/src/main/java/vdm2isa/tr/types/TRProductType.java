118c8347da6653b5125269506a8ccdead91b37b8
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2021-10-21 16:57:32 +0100

    product types; still the invariant bit will be tricky, given the fst/snd business over larger tupples.

--- plugins/vdm2isa/java/src/main/java/vdm2isa/tr/types/TRProductType.java
+++ plugins/vdm2isa/java/src/main/java/vdm2isa/tr/types/TRProductType.java
@@ -17,9 +17,34 @@ public class TRProductType extends TRType {
     }
 
     @Override
-    public String invTranslate(String varName) {
-        return "NYI invTranslate for productType for " + varName;
-    }
+	public String invTranslate(String varName)
+	{
+		StringBuilder sb = new StringBuilder();
+		if (!this.types.isEmpty())
+		{
+			sb.append("(");
+            String fieldVarName = varName == null ? "" : IsaToken.LPAREN.toString() + IsaToken.FST.toString() + " " + varName + IsaToken.RPAREN.toString(); 
+			sb.append(this.types.get(0).invTranslate(fieldVarName));
+
+			for (int i=1; i< this.types.size(); i++)
+			{
+				sb.append(IsaToken.AND.toString());
+				sb.append("\n\t\t");
+                // odd indices are even fields: 
+                // 4 fields = fst x, (fst (snd x)) 
+                //@todo! 
+                //if (i / 2 != 0)
+				sb.append(this.types.get(i).invTranslate(fieldVarName));
+                sb.append(IsaToken.COMMENT.toString());
+                sb.append(IsaToken.COMMENT_OPEN.toString());
+                sb.append("TODO! This is messy, but easy: fst, fst snd, fst snd snd, snd snd snd say for 4-tuple");
+                sb.append(IsaToken.COMMENT_CLOSE.toString());
+                
+			}
+			sb.append(")");
+		}
+		return sb.toString();	
+	}
 
     @Override
     public IsaToken isaToken() {
--- plugins/vdm2isa/java/src/main/java/vdm2isa/tr/types/TRTypeList.java
+++ plugins/vdm2isa/java/src/main/java/vdm2isa/tr/types/TRTypeList.java
@@ -51,10 +51,37 @@ public class TRTypeList extends TRMappedList<TCType, TRType>
 		return IsaTemplates.listToString(this, separator());
 	}
 
+	public String invTranslate(String varName)
+	{
+		StringBuilder sb = new StringBuilder();
+		if (!isEmpty())
+		{
+			sb.append("(");
+
+			sb.append(get(0).invTranslate(varName));
+
+			for (int i=1; i<size(); i++)
+			{
+				sb.append(IsaToken.AND.toString());
+				sb.append("\n\t\t");
+				sb.append(get(i).invTranslate(varName));
+			}
+			sb.append(")");
+		}
+		return sb.toString();	
+	}
+
 	public static String translate(TRType... args)
 	{
 		TRTypeList list = new TRTypeList();
 		list.addAll(Arrays.asList(args));
 		return list.translate();	
+	}
+
+	public static String invTranslate(String varName, TRType... args)
+	{
+		TRTypeList list = new TRTypeList();
+		list.addAll(Arrays.asList(args));
+		return list.invTranslate(varName);	
 	}
 }
