d7f02726a3ed5689618a9a185ddf04247441935f
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2021-10-24 12:25:02 +0100

    TRPattern as single class for whole TCPattern group.

--- plugins/vdm2isa/java/src/main/java/vdm2isa/tr/patterns/TRPattern.java
+++ plugins/vdm2isa/java/src/main/java/vdm2isa/tr/patterns/TRPattern.java
@@ -1,14 +1,20 @@
 package vdm2isa.tr.patterns;
 
 import com.fujitsu.vdmj.lex.LexLocation;
+import com.fujitsu.vdmj.tc.patterns.TCIdentifierPattern;
+import com.fujitsu.vdmj.tc.statements.TCIdentifierDesignator;
 
+import vdm2isa.lex.IsaToken;
 import vdm2isa.tr.TRNode;
 
-public abstract class TRPattern extends TRNode {
+public class TRPattern extends TRNode {
     
-    public TRPattern(LexLocation location)
+    private final String pattern; 
+
+    public TRPattern(TCIdentifierPattern owner)
     {
-        super(location);
+        super(owner.location);
+        pattern = owner.toString();
     }
 
     public TRPatternList getPatternList()
@@ -16,5 +22,15 @@ public abstract class TRPattern extends TRNode {
         TRPatternList list = new TRPatternList();
         list.add(this);
         return list; 
+    }
+
+    @Override
+    public IsaToken isaToken() {
+        return IsaToken.EOF;
+    }
+
+    @Override
+    public String translate() {
+        return pattern;
     }
 }
