f7549e82638e1d458bcc89f1685edbef86640e91
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2021-10-28 06:55:00 +0100

    lambda expressions + remove debug strings

--- plugins/vdm2isa/java/src/main/java/vdm2isa/tr/expressions/TRLambdaExpression.java
+++ plugins/vdm2isa/java/src/main/java/vdm2isa/tr/expressions/TRLambdaExpression.java
@@ -9,6 +9,13 @@ import vdm2isa.tr.patterns.TRPatternList;
 import vdm2isa.tr.patterns.TRTypeBindList;
 import vdm2isa.tr.types.TRFunctionType;
 
+/**
+ * VDM Lambda expressions are complex to translate because of the need for local invariant checks and the need
+ * for TCLambdaExpression type information to be passed through so that we know what kind of lambda TRFunctionType we need.
+ * This is important to ensure that properly curried invariants are added for the lambda, as well as dummy variable 
+ * names for the corresponding lambda invariant check. The TCLambdaExpression TRDefinition def field is not needed
+ * given that paramPatterns/Definitions are sufficient.  
+ */
 public class TRLambdaExpression extends TRExpression {
 
     private TRTypeBindList bindList;
@@ -17,23 +24,35 @@ public class TRLambdaExpression extends TRExpression {
     private TRFunctionType type;
 	private TRPatternList paramPatterns;
 	private TRDefinitionList paramDefinitions;
-	private TRDefinition def;
+	//private TRDefinition def;
 
     public TRLambdaExpression(LexLocation location, TRTypeBindList bindList, TRExpression expression,
-        TRFunctionType type, TRPatternList paramPatterns, TRDefinitionList paramDefinitions, TRDefinition def)
+        TRFunctionType type, TRPatternList paramPatterns, TRDefinitionList paramDefinitions)
     {
         super(location);
         this.bindList = bindList;
         this.expression = expression;
         this.type = type;
+        //TODO refactor these two out as it turns out the use of bind translation is better? 
         this.paramPatterns = paramPatterns; 
         this.paramDefinitions = paramDefinitions; 
+        /*
         System.out.println("LambdaExpr: (lambda " + bindList.translate() + " & " + expression.translate() + " )" +
             "\n\tFcnType    = " + type.translate() +
             "\n\tParamPttrs = " + paramPatterns.translate() + 
             "\n\tParamDefs  = " + paramDefinitions.translate() +
-            "\n\tDef        = " + def.toString() + 
+            //"\n\tDef        = " + def.toString() + 
             "\n");
+        "v74= (lambda var : nat, var2: nat & var + var2);""
+        LambdaExpr: (lambda (var::VDMNat) (var2::VDMNat) & (var + var2) )
+            FcnType    = VDMNat\<Rightarrow>VDMNat \<Rightarrow> VDMNat
+            ParamPttrs = var,var2
+            ParamDefs  = (var::VDMNat)(var2::VDMNat)
+            Def        = vdm2isa.tr.definitions.TRMultiBindListDefinition@5f3b9c57
+                MultiBindListDef
+                    bindings: (var::VDMNat) (var2::VDMNat)
+                    defs    : (var::VDMNat)(var2::VDMNat)
+        */
     }
 
     @Override
@@ -57,6 +76,7 @@ public class TRLambdaExpression extends TRExpression {
         sb.append(isaToken().toString());
         sb.append(" ");
         sb.append(bindList.translate());
+        sb.append(" ");
         sb.append(IsaToken.POINT.toString());
         sb.append("\n\t\t");
         sb.append(IsaToken.LPAREN.toString());
