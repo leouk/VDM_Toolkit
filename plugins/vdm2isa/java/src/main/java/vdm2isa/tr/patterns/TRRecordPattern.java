d3e446140b59128de8b74abbebd67f8a04b09216
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2021-10-30 16:53:54 +0100

    Record patterns are complicated!

---
+++ plugins/vdm2isa/java/src/main/java/vdm2isa/tr/patterns/TRRecordPattern.java
@@ -0,0 +1,89 @@
+package vdm2isa.tr.patterns;
+
+import com.fujitsu.vdmj.lex.LexLocation;
+import com.fujitsu.vdmj.tc.lex.TCNameToken;
+
+import vdm2isa.lex.IsaTemplates;
+import vdm2isa.lex.IsaToken;
+import vdm2isa.tr.patterns.visitors.TRPatternVisitor;
+import vdm2isa.tr.types.TRType;
+
+public class TRRecordPattern extends TRPattern {
+    
+    private final TCNameToken typename;
+    private final TRPatternList plist;
+    private final TRType type;
+    
+    public TRRecordPattern(LexLocation location, TCNameToken typename, TRPatternList plist, TRType type)
+    {
+        super(location);
+        this.typename = typename;
+        this.plist = plist;
+        this.type = type;
+        if (this.plist.size() == 0)
+            report(10002, "Isabelle does not allow empty patterns for VDM record pattern for record type " + typename.toString());
+
+        //System.out.println(toString());
+    }
+
+    @Override
+    public String toString()
+    {
+        return super.toString() + " tname " + typename.toString() + " plist = " + plist.translate() + " type = " + type.translate();
+    }
+
+    @Override
+    public IsaToken isaToken() {
+        return IsaToken.RECORD;
+    }
+
+    /**
+     * Record patterns translate to dummy name, and its invTranslate considers rest? 
+     */
+    @Override
+    public String translate() {
+        return IsaToken.dummyVarNames(1, location);
+    }
+
+    protected String fieldNameTranslate(int index, String dummyName)
+    {
+        assert index >= 0 && index < plist.size();
+        StringBuilder sb = new StringBuilder();
+        // only leaf patterns have invTranslate, so this is "safe".
+        String fieldName = plist.get(index).invTranslate();
+    
+        sb.append(fieldName);
+        sb.append(" ");
+        sb.append(IsaToken.EQUALS.toString());
+        sb.append(" ");
+        sb.append(IsaToken.parenthesise(IsaTemplates.isabelleRecordFieldName(typename.toString(), fieldName) + " " + dummyName));
+        return sb.toString();
+    }
+
+    /**
+     * On the actual record pattern, invTranslate its TRPatternList with SEMICOLONS. This sets up the 
+     * local declaration context to unpick projected fields. The TRPatternList.recordPatternTranslate()
+     * call will handle let-in and parenthesis.   
+     * @return
+     */
+    public String recordPatternTranslate()
+    {
+        StringBuilder sb = new StringBuilder();
+        String dummyName = translate();
+		if (!plist.isEmpty())
+		{
+            sb.append(fieldNameTranslate(0, dummyName));
+            for (int i=1; i < plist.size(); i++)
+			{
+                sb.append(IsaToken.SEMICOLON.toString() + " ");
+                sb.append(fieldNameTranslate(i, dummyName));
+			}
+		}
+		return sb.toString();
+    }
+
+    @Override
+    public <R, S> R apply(TRPatternVisitor<R, S> visitor, S arg) {
+        return visitor.caseRecordPattern(this, arg);
+    }
+}
