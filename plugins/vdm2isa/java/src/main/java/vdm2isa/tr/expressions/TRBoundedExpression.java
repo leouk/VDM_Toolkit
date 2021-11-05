85fe7e725606a51a091af676af0ce937128b18a7
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2021-11-05 08:50:00 +0000

    complete simplification of bounded expressions to take multiple bind handling per pattern into account, hence allowing individual quantifiers per pattern.

--- plugins/vdm2isa/java/src/main/java/vdm2isa/tr/expressions/TRBoundedExpression.java
+++ plugins/vdm2isa/java/src/main/java/vdm2isa/tr/expressions/TRBoundedExpression.java
@@ -1,5 +1,8 @@
 package vdm2isa.tr.expressions;
 
+import java.util.Map;
+import java.util.SortedSet;
+
 import com.fujitsu.vdmj.tc.expressions.TCExists1Expression;
 import com.fujitsu.vdmj.tc.expressions.TCExistsExpression;
 import com.fujitsu.vdmj.tc.expressions.TCForAllExpression;
@@ -8,9 +11,11 @@ import vdm2isa.lex.IsaTemplates;
 import vdm2isa.lex.IsaToken;
 import vdm2isa.tr.expressions.visitors.TRExpressionVisitor;
 import vdm2isa.tr.patterns.TRMultipleBind;
+import vdm2isa.tr.patterns.TRMultipleBindKind;
 import vdm2isa.tr.patterns.TRMultipleBindList;
 import vdm2isa.tr.patterns.TRMultipleSetBind;
 import vdm2isa.tr.patterns.TRMultipleTypeBind;
+import vdm2isa.tr.patterns.TRPattern;
 
 public class TRBoundedExpression extends TRExpression {
     
@@ -18,6 +23,7 @@ public class TRBoundedExpression extends TRExpression {
 	private final TRMultipleBindList bindList;
 	private final TRExpression predicate;
     private final IsaToken owner;
+    private int rParenCount;
      
     public TRBoundedExpression(TCExists1Expression owner, TRMultipleBind bind, TRExpression predicate)
     {
@@ -47,9 +53,10 @@ public class TRBoundedExpression extends TRExpression {
     protected void setup()
 	{
         super.setup();
+        rParenCount = 0;
 	// 	setSemanticSeparator("");
 	 	setFormattingSeparator(" ");
-	// 	setInvTranslateSeparator("");
+	 	setInvTranslateSeparator(getFormattingSeparator() + IsaToken.IMPLIES.toString() + getFormattingSeparator());
 	}
 
     @Override
@@ -65,49 +72,37 @@ public class TRBoundedExpression extends TRExpression {
         else
            return super.tldIsaComment();
     }
-
-    /**
-     * If bind translating with indexes, there will be various quantified formulae! 
-     * This should only be called if the binds list size is greater than 1.
-     * @param index
-     * @return
-     */
-    protected String bindTranslate(int index)
-    {
-        StringBuilder sb = new StringBuilder();
-        //if (bindList.size() <= 1)
-        //    report(11111, "Translation of multiple binds in \"" + isaToken().vdmToken().toString() + "\" expression bind list must contain at least two binds.");
-        //else 
-        //if (index >= bindList.size() - 1)
-        //    report(11111, "Translation of multiple binds in \"" + isaToken().vdmToken().toString() + "\" expression must not include the last bind (" + index + ").");
-        //else
-        //{
-            // should this be pushed upwards towards bindlist? Or not because of the context for bounded quantifier? 
-            assert index >= 0 && index < bindList.size() - 1;
-            // first LPAREN is done externally
-            if (index > 0)
-            {
-                sb.append(IsaToken.LPAREN.toString());
-            }
-            // translate the quantifier and the specific bind
-            sb.append(isaToken().toString());
-            sb.append(getFormattingSeparator());
-            // VDM AST has the bind list within the set/seq binds themselves!
-            //TODO perhaps push this into TRMultipleSetBind? 
-            sb.append(bindList.get(index).translate());
-            sb.append(getFormattingSeparator());
-            sb.append(IsaToken.POINT.toString());
-            sb.append(getFormattingSeparator());
-            // do not translate predicate or RPAREN
-        //}
-        return sb.toString();
-    }
-
+    
     @Override
     public boolean requiresImplicitTypeInvariantChecks()
     {
         // if found type binds, issue comment about extra invariant checks for subtype soundness
-        return bindList.foundSomeTypeBinds();
+        return bindList.foundBinds(TRMultipleBindKind.TYPE);
+    }
+
+        /**
+     * [quantifier] + [bindings] + [.] formatting separated
+     */
+    protected String boundedPatternsTranslate(String patterns)
+    {
+        assert patterns != null && !patterns.isEmpty();
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(IsaToken.LPAREN.toString());
+        sb.append(isaToken().toString());
+        sb.append(getFormattingSeparator());
+        
+        sb.append(patterns);
+        sb.append(getFormattingSeparator());
+
+        sb.append(getFormattingSeparator());
+        sb.append(IsaToken.POINT.toString());
+        sb.append(getFormattingSeparator());
+
+        // do not translate predicate or RPAREN; caller must issue rParenCount  
+        rParenCount++;
+        return sb.toString();
     }
 
     /**
@@ -120,69 +115,58 @@ public class TRBoundedExpression extends TRExpression {
         // bindList.translate() + IsaToken.POINT + " " + predicate.translate());
         StringBuilder sb = new StringBuilder();
 
-        // figure out what kind of translation this will be
-        //TODO could this logic be simplified? Probably not given bind kinds can mix and are complicated to enterily classify
-        boolean foundNonTypeBinds = bindList.foundNonTypeBinds();
-        boolean foundTypeBind = bindList.foundSomeTypeBinds();
+        // restart the RPAREN count 
+        this.rParenCount = 0;
+        boolean oldP = bindList.setParenthesise(false);
 
-        if (foundTypeBind) 
+        // if only type binds, no need for multiple quantifiers
+        if (bindList.areBindsUniform(TRMultipleBindKind.TYPE))
         {
-            sb.append(IsaToken.comment("Implicitly defined type invariant checks for quantified type binds", getFormattingSeparator()));
-        }
-
-        sb.append(IsaToken.LPAREN.toString());
-        // count the number of extra left parenthesis that will be added; cout=0 doesn't given it's outermost
-        int rightParenCountToAdd = 0; 
-        if (foundNonTypeBinds)
-        {
-            for(; rightParenCountToAdd < bindList.size(); rightParenCountToAdd++)
-            {
-                sb.append(bindTranslate(rightParenCountToAdd));
-            }
-        }
-        else
-        {
-            // translate the quantifier and the specific bind
-            // lists with type binds only don't need multiple quantifiers
-            // but will need the final right parenthesis, so increase count
-            rightParenCountToAdd++; 
-            sb.append(isaToken().toString());
-            sb.append(getFormattingSeparator());
+            // type binds require parenthesis! 
+            // e.g. "forall x in set S & P" => "! (x : S) . P" is wrong! Has to be "! x : S . P" for set binds, whereas
+            //      "forall x:T & P" => "! x::T . P" is wrong! Has to be "! (x::T) . P" for type binds 
             
-            sb.append(bindList.translate());
-            sb.append(getFormattingSeparator());
-
-            sb.append(getFormattingSeparator());
-            sb.append(IsaToken.POINT.toString());
-
-            sb.append(getFormattingSeparator());
+            //spurious now with getParenthesise() in type bind always returning true, but leave for clarity
+            bindList.setParenthesise(true);
+            sb.append(boundedPatternsTranslate(bindList.translate()));
+        }
+        // if mixed binds, then will have multiple quantifiers, e.g. x:nat, y in set S => ! x::VDMNat . ! y : S
+        else 
+        {
+            // for each bind individually translate each pattern
+            for(int i = 0; i < bindList.size(); i++)
+            {
+                for(int j = 0; j < bindList.get(i).plist.size(); j++)
+                {
+                    sb.append(boundedPatternsTranslate(bindList.get(i).patternTranslate(j, false)));
+                }
+            }
         }
 
         // if found type bind add implication to predicate and an extra parenthesis pair
-        // this more involved checks account for the mixed case, 
         // e.g. forall x: nat, y: nat, z in set S & P(x,y,z)
-        if (foundTypeBind)
+        if (requiresImplicitTypeInvariantChecks())
         {
             sb.append(IsaToken.LPAREN.toString());
-            rightParenCountToAdd++;
+            rParenCount++;
 
             // type binds require invariant checks before the user predicate!
             // presume the bind list has the right inv separator in place  
-            // for mixed type + set/seq binds, this gets invovled! 
+            // for mixed type + set/seq binds, add inv checks for each bind kind! 
             sb.append(bindList.invTranslate());
-            sb.append(getFormattingSeparator());
-            sb.append(IsaToken.IMPLIES.toString());
-            sb.append(getFormattingSeparator());
+            sb.append(getInvTranslateSeparator());
         }
+        // add the bounded predicate
         sb.append(predicate.translate());
-        // if one required bind went through, it will add its own LPAREN, so close the last;
-        // if >1 was required, then we will need count-1 RPAREN closed please
-        if (rightParenCountToAdd > 1)
+
+        // otherwise we would have empty binds, which will be caught within TRMultipleBindList. Avoid reissuing the error
+        if (this.rParenCount > 0)
         {
-            sb.append(IsaTemplates.replicate(IsaToken.RPAREN.toString(), rightParenCountToAdd-1));
+            sb.append(IsaTemplates.replicate(IsaToken.RPAREN.toString(), this.rParenCount));
         }
-        // final overal expression right parenthesis
-        sb.append(IsaToken.RPAREN.toString());
+
+        bindList.setParenthesise(oldP);
+
         return sb.toString();
     }
 
