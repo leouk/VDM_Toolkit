9efaf74875636e7c588b0647004a54b99e10390b
Leo Freitas <leo.freitas@newcastle.ac.uk>
2021-11-07 15:15:26 +0000

    start settting up proper plugin property management

---
+++ plugins/vdm2isa/java/src/main/java/plugins/IsaProperties.java
@@ -0,0 +1,8 @@
+package plugins;
+
+import com.fujitsu.vdmj.config.Properties;
+
+public class IsaProperties extends Properties {
+    
+    //TODO get the stuff from plugs in here and integrate with VDMJs properties management 
+}
