cfb170ec1e430d0c58e27cc5603c9a1a586ef2d2
Leo Freitas <leo.freitas@newcastle.ac.uk>
2021-11-08 08:24:16 +0000

    example to test

---
+++ plugins/vdm2isa/java/src/test/resources/TestV2ITypes.thy
@@ -0,0 +1,14 @@
+(* VDM to Isabelle Translated
+   Copyright 2021, Leo Freitas, leo.freitas@newcastle.ac.uk
+
+in './src/test/resources/TestV2ITypes.vdmsl' at line 1:8files = [./src/test/resources/TestV2ITypes.vdmsl]
+*)
+theory TestV2ITypes
+imports VDMToolkit
+begin
+
+
+
+	
+
+end
