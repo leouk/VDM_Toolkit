d342a1aeab4732455f34bfad8e7aac334ecd4b47
Leo Freitas <leo.freitas@newcastle.ac.uk>
2021-11-09 08:05:42 +0000

    funny function type spec

--- plugins/vdm2isa/java/src/test/resources/TestV2ITypes.thy
+++ plugins/vdm2isa/java/src/test/resources/TestV2ITypes.thy
@@ -8,7 +8,54 @@ imports VDMToolkit
 begin
 
 
+type_synonym TBasic = "TBasic = VDMNat1"
+	
+	definition
+	inv_TBasic :: " \<Rightarrow> \<bool>"
+where
+	"inv_TBasic  \<equiv> inv_TBasic  \<and> "
 
 	
+type_synonym TSet = "TSet = TBasic = VDMNat1 VDMSet"
+	
+	definition
+	inv_TSet :: " \<Rightarrow> \<bool>"
+where
+	"inv_TSet  \<equiv> inv_TSet  \<and> "
 
+	
+type_synonym TOptional = "TOptional = VDMNat1\<Rightarrow>option"
+	
+	definition
+	inv_TOptional :: " \<Rightarrow> \<bool>"
+where
+	"inv_TOptional  \<equiv> inv_TOptional  \<and> "
+
+	
+type_synonym TFcn = "TFcn = VDMNat1 \<Rightarrow> VDMNat1"
+	
+	definition
+	inv_TFcn :: " \<Rightarrow> \<bool>"
+where
+	"inv_TFcn  \<equiv> inv_TFcn  \<and> "
+
+	
+abbreviation
+	v :: "TFcn = VDMNat1 \<Rightarrow> VDMNat1"
+where
+	"v \<equiv> (
+	\<lambda> (x :: VDMNat1) .
+		(if (((inv_VDMNat1 x))) then
+		((x * x) + (10::VDMNat1))
+	 else
+		undefined
+	)
+	)"
+
+	definition
+	inv_v :: "\<bool>"
+where
+	"inv_v  \<equiv> (inv_TFcn v)"
+
+	
 end
