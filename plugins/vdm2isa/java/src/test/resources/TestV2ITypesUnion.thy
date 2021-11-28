87d03826d299bcbc8d985dcdb8866b9796479d3e
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2021-11-28 11:48:59 +0000

    concrete example where type coercion / inferencing will be needed to know whether to coerce or prune the union selection. When regenerated, will need to "format" to know where to delete!

--- plugins/vdm2isa/java/src/test/resources/TestV2ITypesUnion.thy
+++ plugins/vdm2isa/java/src/test/resources/TestV2ITypesUnion.thy
@@ -1,20 +1,20 @@
-(* VDM to Isabelle Translation @2021-11-28T10:01:52.588Z
+(* VDM to Isabelle Translation @2021-11-28T11:15:11.880815Z
    Copyright 2021, Leo Freitas, leo.freitas@newcastle.ac.uk
 
-in '/Users/nljsf/Local/reps/git/VDM_Toolkit/plugins/vdm2isa/java/src/test/resources/TestV2ITypesUnion.vdmsl' at line 1:8
-files = [/Users/nljsf/Local/reps/git/VDM_Toolkit/plugins/vdm2isa/java/src/test/resources/TestV2ITypesUnion.vdmsl]
+in './src/test/resources/TestV2ITypesUnion.vdmsl' at line 1:8
+files = [./src/test/resources/TestV2ITypesUnion.vdmsl]
 *)
 theory TestV2ITypesUnion
 imports VDMToolkit
 begin
 
 
-\<comment>\<open>in 'TestV2ITypesUnion' (/Users/nljsf/Local/reps/git/VDM_Toolkit/plugins/vdm2isa/java/src/test/resources/TestV2ITypesUnion.vdmsl) at line 6:5\<close>
+\<comment>\<open>in 'TestV2ITypesUnion' (./src/test/resources/TestV2ITypesUnion.vdmsl) at line 6:5\<close>
 datatype TUnion2 = U_VDMNat_VDMSeq "VDMNat VDMSeq" | 
 		 U_VDMReal_VDMSet "VDMReal VDMSet"
 	
 
-\<comment>\<open>in 'TestV2ITypesUnion' (/Users/nljsf/Local/reps/git/VDM_Toolkit/plugins/vdm2isa/java/src/test/resources/TestV2ITypesUnion.vdmsl) at line 6:5\<close>
+\<comment>\<open>in 'TestV2ITypesUnion' (./src/test/resources/TestV2ITypesUnion.vdmsl) at line 6:5\<close>
 definition
 	inv_TUnion2 :: "TUnion2 \<Rightarrow> \<bool>"
 where
@@ -27,12 +27,12 @@ where
  
 
 	
-\<comment>\<open>in 'TestV2ITypesUnion' (/Users/nljsf/Local/reps/git/VDM_Toolkit/plugins/vdm2isa/java/src/test/resources/TestV2ITypesUnion.vdmsl) at line 7:5\<close>
+\<comment>\<open>in 'TestV2ITypesUnion' (./src/test/resources/TestV2ITypesUnion.vdmsl) at line 7:5\<close>
 datatype TUnion2' = U_VDMNat_VDMSeq "VDMNat VDMSeq" | 
 		 U_VDMReal_VDMSeq "VDMReal VDMSeq"
 	
 
-\<comment>\<open>in 'TestV2ITypesUnion' (/Users/nljsf/Local/reps/git/VDM_Toolkit/plugins/vdm2isa/java/src/test/resources/TestV2ITypesUnion.vdmsl) at line 7:5\<close>
+\<comment>\<open>in 'TestV2ITypesUnion' (./src/test/resources/TestV2ITypesUnion.vdmsl) at line 7:5\<close>
 definition
 	inv_TUnion2' :: "TUnion2' \<Rightarrow> \<bool>"
 where
@@ -45,11 +45,11 @@ where
  
 
 	
-\<comment>\<open>in 'TestV2ITypesUnion' (/Users/nljsf/Local/reps/git/VDM_Toolkit/plugins/vdm2isa/java/src/test/resources/TestV2ITypesUnion.vdmsl) at line 8:5\<close>
+\<comment>\<open>in 'TestV2ITypesUnion' (./src/test/resources/TestV2ITypesUnion.vdmsl) at line 8:5\<close>
 datatype TUnion6 = U_VDMInt "VDMInt"
 	
 
-\<comment>\<open>in 'TestV2ITypesUnion' (/Users/nljsf/Local/reps/git/VDM_Toolkit/plugins/vdm2isa/java/src/test/resources/TestV2ITypesUnion.vdmsl) at line 9:9\<close>
+\<comment>\<open>in 'TestV2ITypesUnion' (./src/test/resources/TestV2ITypesUnion.vdmsl) at line 9:9\<close>
 definition
 	inv_TUnion6 :: "TUnion6 \<Rightarrow> \<bool>"
 where
@@ -61,15 +61,15 @@ where
 		\<comment>\<open>User defined body of inv_TUnion6\<close>
 		(
 		\<comment>\<open>Implicit union type parameters projection conversion\<close>
-		case u of
+		(case u of
 			U_VDMInt u \<Rightarrow> ((isTest (u) (inv_VDMNat) \<longrightarrow> (u > 0)) \<and> (isTest (u) (inv_VDMInt) \<longrightarrow> (u < 0)))
-		 )"
+		 ))"
  
 
 	
-\<comment>\<open>in 'TestV2ITypesUnion' (/Users/nljsf/Local/reps/git/VDM_Toolkit/plugins/vdm2isa/java/src/test/resources/TestV2ITypesUnion.vdmsl) at line 13:5\<close>
+\<comment>\<open>in 'TestV2ITypesUnion' (./src/test/resources/TestV2ITypesUnion.vdmsl) at line 13:5\<close>
 
-\<comment>\<open>in 'TestV2ITypesUnion' (/Users/nljsf/Local/reps/git/VDM_Toolkit/plugins/vdm2isa/java/src/test/resources/TestV2ITypesUnion.vdmsl) at line 13:5\<close>
+\<comment>\<open>in 'TestV2ITypesUnion' (./src/test/resources/TestV2ITypesUnion.vdmsl) at line 13:5\<close>
 definition
 	pre_f4 :: "TUnion6\<Rightarrow> TUnion2 \<Rightarrow> \<bool>"
 where
@@ -78,7 +78,7 @@ where
 		((inv_TUnion6 u6)  \<and>  (inv_TUnion2 u2))"
 
 
-\<comment>\<open>in 'TestV2ITypesUnion' (/Users/nljsf/Local/reps/git/VDM_Toolkit/plugins/vdm2isa/java/src/test/resources/TestV2ITypesUnion.vdmsl) at line 13:5\<close>
+\<comment>\<open>in 'TestV2ITypesUnion' (./src/test/resources/TestV2ITypesUnion.vdmsl) at line 13:5\<close>
 definition
 	post_f4 :: "TUnion6\<Rightarrow> TUnion2\<Rightarrow> \<bool> \<Rightarrow> \<bool>"
 where
@@ -93,23 +93,40 @@ where
 	\<comment>\<open>User defined body of f4\<close>
 	(
 	\<comment>\<open>Implicit union type parameters projection conversion\<close>
-	case u2 of
-			TUnion2.U_VDMNat_VDMSeq u2 \<Rightarrow> ((isTest (u6) (inv_VDMInt) \<longrightarrow> ((isTest (u2) (inv_VDMSeq' (inv_VDMNat)) 
-      \<longrightarrow> (u6 \<in> (elems u2))) \<and> (isTest (u2) (inv_VDMSet' (inv_VDMReal)) \<longrightarrow> (u6 \<in> u2)))) \<and> (isTest (u6) (inv_VDMNat) 
-          \<longrightarrow> ((isTest (u2) (inv_VDMSeq' (inv_VDMNat)) \<longrightarrow> (\<not> (u6 \<in> (elems u2)))) \<and> (isTest (u2) (inv_VDMSet' (inv_VDMReal)) \<longrightarrow> (\<not> (u6 \<in> u2))))))
-		  | TUnion2.U_VDMReal_VDMSet u2 \<Rightarrow> ((isTest (u6) (inv_VDMInt) \<longrightarrow> ((isTest (u2) (inv_VDMSeq' (inv_VDMNat)) 
-      \<longrightarrow> (u6 \<in> (elems u2))) \<and> (isTest (u2) (inv_VDMSet' (inv_VDMReal)) \<longrightarrow> (u6 \<in> u2)))) \<and> (isTest (u6) (inv_VDMNat) 
-          \<longrightarrow> ((isTest (u2) (inv_VDMSeq' (inv_VDMNat)) \<longrightarrow> (\<not> (u6 \<in> (elems u2)))) \<and> (isTest (u2) (inv_VDMSet' (inv_VDMReal)) \<longrightarrow> (\<not> (u6 \<in> u2))))))
-		 case u6 of
-	        		TUnion6.U_VDMInt u6 \<Rightarrow> ((isTest (u6) (inv_VDMInt) \<longrightarrow> ((isTest (u2) (inv_VDMSeq' (inv_VDMNat)) 
-      \<longrightarrow> (u6 \<in> (elems u2))) \<and> (isTest (u2) (inv_VDMSet' (inv_VDMReal)) \<longrightarrow> (u6 \<in> u2)))) \<and> (isTest (u6) (inv_VDMNat) 
-          \<longrightarrow> ((isTest (u2) (inv_VDMSeq' (inv_VDMNat)) \<longrightarrow> (\<not> (u6 \<in> (elems u2)))) \<and> (isTest (u2) (inv_VDMSet' (inv_VDMReal)) \<longrightarrow> (\<not> (u6 \<in> u2))))))
-		 )"
+	(case u2 of
+			TUnion2.U_VDMNat_VDMSeq u2 \<Rightarrow> 
+      (case u6 of
+          TUnion6.U_VDMInt u6 \<Rightarrow> 
+              ((isTest (u6) (inv_VDMInt) \<longrightarrow> 
+                  ( (isTest (u2) (inv_VDMSeq' (inv_VDMNat)) \<longrightarrow> (u6 \<in> (elems u2)))  
+                   \<comment> \<open> \<and> (isTest (u2) (inv_VDMSet' (inv_VDMReal)) \<longrightarrow> (u6 \<in> u2)) \<close>
+                  )
+                ) \<and> 
+              (isTest (u6) (inv_VDMNat) \<longrightarrow> 
+                  ( (isTest (u2) (inv_VDMSeq' (inv_VDMNat)) \<longrightarrow> (\<not> (u6 \<in> (elems u2)))) 
+                    \<comment> \<open> \<and> (isTest (u2) (inv_VDMSet' (inv_VDMReal)) \<longrightarrow> (\<not> (u6 \<in> u2))) \<close>
+                  )
+              ))
+       )
+		  | TUnion2.U_VDMReal_VDMSet u2 \<Rightarrow> 
+      (case u6 of
+            TUnion6.U_VDMInt u6 \<Rightarrow> 
+              ((isTest (u6) (inv_VDMInt) \<longrightarrow> 
+                  ( \<comment> \<open>(isTest (u2) (inv_VDMSeq' (inv_VDMNat)) \<longrightarrow> (u6 \<in> (elems u2))) \<and> \<close>
+                   (isTest (u2) (inv_VDMSet' (inv_VDMReal)) \<longrightarrow> (u6 \<in> u2))
+                  )) 
+                  \<and> 
+               (isTest (u6) (inv_VDMNat) \<longrightarrow> 
+                  ( \<comment> \<open>(isTest (u2) (inv_VDMSeq' (inv_VDMNat)) \<longrightarrow> (\<not> (u6 \<in> (elems u2)))) \<and>\<close> 
+                   (isTest (u2) (inv_VDMSet' (inv_VDMReal)) \<longrightarrow> (\<not> (u6 \<in> u2))))
+                 ))
+           )
+		 ))"
 
 	
-\<comment>\<open>in 'TestV2ITypesUnion' (/Users/nljsf/Local/reps/git/VDM_Toolkit/plugins/vdm2isa/java/src/test/resources/TestV2ITypesUnion.vdmsl) at line 32:5\<close>
+\<comment>\<open>in 'TestV2ITypesUnion' (./src/test/resources/TestV2ITypesUnion.vdmsl) at line 32:5\<close>
 
-\<comment>\<open>in 'TestV2ITypesUnion' (/Users/nljsf/Local/reps/git/VDM_Toolkit/plugins/vdm2isa/java/src/test/resources/TestV2ITypesUnion.vdmsl) at line 32:5\<close>
+\<comment>\<open>in 'TestV2ITypesUnion' (./src/test/resources/TestV2ITypesUnion.vdmsl) at line 32:5\<close>
 definition
 	pre_f4' :: "TUnion6\<Rightarrow> TUnion2' \<Rightarrow> \<bool>"
 where
@@ -118,7 +135,7 @@ where
 		((inv_TUnion6 u6)  \<and>  (inv_TUnion2' u2))"
 
 
-\<comment>\<open>in 'TestV2ITypesUnion' (/Users/nljsf/Local/reps/git/VDM_Toolkit/plugins/vdm2isa/java/src/test/resources/TestV2ITypesUnion.vdmsl) at line 32:5\<close>
+\<comment>\<open>in 'TestV2ITypesUnion' (./src/test/resources/TestV2ITypesUnion.vdmsl) at line 32:5\<close>
 definition
 	post_f4' :: "TUnion6\<Rightarrow> TUnion2'\<Rightarrow> \<bool> \<Rightarrow> \<bool>"
 where
@@ -133,11 +150,13 @@ where
 	\<comment>\<open>User defined body of f4'\<close>
 	(
 	\<comment>\<open>Implicit union type parameters projection conversion\<close>
-	case u2 of
-			TUnion2'.U_VDMNat_VDMSeq u2 \<Rightarrow> (u6 \<in> (elems u2))
-		  | TUnion2'.U_VDMReal_VDMSeq u2 \<Rightarrow> (u6 \<in> (elems u2))
-		 case u6 of
+	(case u2 of
+			TUnion2'.U_VDMNat_VDMSeq u2 \<Rightarrow> (case u6 of
 			TUnion6.U_VDMInt u6 \<Rightarrow> (u6 \<in> (elems u2))
-		 )"
+		 )
+		  | TUnion2'.U_VDMReal_VDMSeq u2 \<Rightarrow> (case u6 of
+			TUnion6.U_VDMInt u6 \<Rightarrow> (u6 \<in> (elems u2))
+		 )
+		 ))"
 
 end
