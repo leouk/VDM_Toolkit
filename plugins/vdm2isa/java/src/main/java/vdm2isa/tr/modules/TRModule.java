95fffc1bf1d494ee1502a34fc35fc9751ed82828
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2021-12-11 13:31:34 +0000

    process module imports. Nice.

--- plugins/vdm2isa/java/src/main/java/vdm2isa/tr/modules/TRModule.java
+++ plugins/vdm2isa/java/src/main/java/vdm2isa/tr/modules/TRModule.java
@@ -4,37 +4,59 @@
 
 package vdm2isa.tr.modules;
 
+import com.fujitsu.vdmj.tc.annotations.TCAnnotationList;
 import com.fujitsu.vdmj.tc.lex.TCIdentifierToken;
+import com.fujitsu.vdmj.tc.modules.TCImportFromModule;
 import com.fujitsu.vdmj.tc.modules.TCModule;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
 
 import com.fujitsu.vdmj.lex.LexLocation;
 import com.fujitsu.vdmj.mapper.FileList;
 
 import vdm2isa.lex.IsaTemplates;
 import vdm2isa.lex.IsaToken;
+import vdm2isa.lex.TRIsaVDMCommentList;
 import vdm2isa.messages.IsaErrorMessage;
 import vdm2isa.messages.IsaWarningMessage;
 import vdm2isa.tr.TRNode;
+import vdm2isa.tr.definitions.TRDefinition;
 import vdm2isa.tr.definitions.TRDefinitionList;
+import vdm2isa.tr.definitions.TRRenamedDefinition;
 
 public class TRModule extends TRNode
 {
 	private static final long serialVersionUID = 1L;
 	public final TCIdentifierToken name;
 	public final TRDefinitionList definitions;
+	public final TRDefinitionList importdefs;
+	public final TRDefinitionList exportdefs;
+	public final TRIsaVDMCommentList comments;
+	public final TCAnnotationList annotations; 
 	public final FileList files;
-	public final TCModule module; 
+	public final TCModule module;
 
-	public TRModule(TCModule m, TCIdentifierToken name, TRDefinitionList definitions, FileList files)
+	private final Set<String> imports; 
+	private final TRDefinitionList allDefs;
+
+	public TRModule(TCModule m, TRIsaVDMCommentList comments, TCAnnotationList annotations, TCIdentifierToken name, 
+		TRDefinitionList importdefs, TRDefinitionList exportdefs, TRDefinitionList definitions, FileList files)
 	{
 		super(name != null ? name.getLocation() : LexLocation.ANY);
 		this.module = m;
-		this.name = name;
+		this.comments = comments != null ? comments : new TRIsaVDMCommentList();
+		this.annotations = annotations; 
+		this.name = name == null ? TCModule.defaultName(location) : name;
+		this.importdefs = importdefs;
+		this.exportdefs = exportdefs;
 		this.definitions = definitions;
 		this.files = files;
+
+		this.imports = new TreeSet<String>();
+		this.allDefs = new TRDefinitionList();
 	}
 
 	@Override 
@@ -63,12 +85,37 @@ public class TRModule extends TRNode
 			}
 			if (!found)
 			{
-				report(IsaErrorMessage.ISA_INVALID_MODULE_NAME_2P, name.toString(), files.toString());
+				warning(IsaWarningMessage.ISA_INVALID_MODULE_NAME_2P, name.toString(), files.toString());
 			}
 		}
-		
-		TRNode.setup(definitions);
-		System.out.println(toString());
+		figureOutModuleImports();
+		allDefs.addAll(definitions);
+		TRNode.setup(allDefs);
+		//System.out.println(toString());
+	}
+
+	private void figureOutModuleImports()
+	{
+		this.imports.add(IsaToken.VDMTOOLKIT.toString());
+		if (module.imports != null && module.imports.imports != null)
+		{
+			for(TCImportFromModule i : module.imports.imports)
+			{
+				this.imports.add(i.name.toString());
+			}
+		}
+		if (importdefs != null)
+		{
+			// make sure imports are processed first
+			assert allDefs.isEmpty();
+			for(TRDefinition d : importdefs)
+			{
+				if (d instanceof TRRenamedDefinition)
+				{
+					allDefs.add(d);
+				}
+			}
+		}
 	}
 
 	@Override 
@@ -85,13 +132,28 @@ public class TRModule extends TRNode
 	}
 
 	/**
-	 * If multiple imports list must take into account Isabelle impoting syntax 
-	 * (i.e. innerSyntaxIt if relative path; just space separated string otherwise)
+	 * If multiple imports list must take into account Isabelle importing syntax 
+	 * (i.e. innerSyntax It if relative path; just space separated string otherwise);
 	 * @return
 	 */
 	public String getImports()
 	{
-		return IsaToken.VDMTOOLKIT.toString();
+		assert imports.contains(IsaToken.VDMTOOLKIT.toString());
+		StringBuilder sb = new StringBuilder();
+		for(String i : imports)
+		{
+			sb.append(requiresQuotes(i) ? IsaToken.bracketit(IsaToken.ISAQUOTE, i, IsaToken.ISAQUOTE) : i);
+			sb.append(IsaToken.SPACE.toString());
+		}
+		return sb.toString();
+	}
+
+	private final boolean requiresQuotes(String importName)
+	{
+		return importName.indexOf(File.pathSeparator) != -1 || 
+				importName.indexOf(File.separator) != -1 || 
+				!importName.equals(IsaToken.VDMTOOLKIT.toString()) ||
+				!IsaToken.validIsaIdentifier(importName); 
 	}
 
 	@Override
@@ -102,7 +164,7 @@ public class TRModule extends TRNode
 		{ 
 			loc += "\nfiles = " + files.toString();
 		}
-		return IsaTemplates.translateModule("", loc, name.toString(), getImports(), definitions.translate());
+		return IsaTemplates.translateModule("", loc, name.toString(), getImports(), allDefs.translate());
 	}
 
     /**
