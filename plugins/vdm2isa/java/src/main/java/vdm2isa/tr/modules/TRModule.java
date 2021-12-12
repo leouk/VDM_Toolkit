aed9467a3d80d280733a190d72bc8ae0f145f60a
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2021-12-12 10:59:16 +0000

    process exports as definitions to hide from module defined ones.

--- plugins/vdm2isa/java/src/main/java/vdm2isa/tr/modules/TRModule.java
+++ plugins/vdm2isa/java/src/main/java/vdm2isa/tr/modules/TRModule.java
@@ -4,12 +4,27 @@
 
 package vdm2isa.tr.modules;
 
+import com.fujitsu.vdmj.tc.definitions.TCDefinition;
+import com.fujitsu.vdmj.tc.definitions.TCDefinitionList;
+import com.fujitsu.vdmj.tc.definitions.TCExplicitFunctionDefinition;
+import com.fujitsu.vdmj.tc.definitions.TCExplicitOperationDefinition;
+import com.fujitsu.vdmj.tc.definitions.TCImplicitFunctionDefinition;
+import com.fujitsu.vdmj.tc.definitions.TCImplicitOperationDefinition;
+import com.fujitsu.vdmj.tc.definitions.TCLocalDefinition;
+import com.fujitsu.vdmj.tc.definitions.TCQualifiedDefinition;
+import com.fujitsu.vdmj.tc.definitions.TCRenamedDefinition;
+import com.fujitsu.vdmj.tc.definitions.TCTypeDefinition;
+import com.fujitsu.vdmj.tc.definitions.TCValueDefinition;
 import com.fujitsu.vdmj.tc.lex.TCIdentifierToken;
+import com.fujitsu.vdmj.tc.modules.TCExport;
+import com.fujitsu.vdmj.tc.modules.TCExportAll;
 import com.fujitsu.vdmj.tc.modules.TCImportFromModule;
 import com.fujitsu.vdmj.tc.modules.TCModule;
+import com.fujitsu.vdmj.tc.types.TCType;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -20,6 +35,7 @@ import vdm2isa.lex.IsaTemplates;
 import vdm2isa.lex.IsaToken;
 import vdm2isa.lex.TRIsaVDMCommentList;
 import vdm2isa.messages.IsaErrorMessage;
+import vdm2isa.messages.IsaInfoMessage;
 import vdm2isa.messages.IsaWarningMessage;
 import vdm2isa.tr.TRNode;
 import vdm2isa.tr.annotations.TRAnnotationList;
@@ -40,6 +56,7 @@ public class TRModule extends TRNode
 	public final TCModule module;
 
 	private final Set<String> imports; 
+	private final Set<TCDefinition> defsToHide;
 	private final TRDefinitionList allDefs;
 
 	public TRModule(TCModule m, TRIsaVDMCommentList comments, TRAnnotationList annotations, TCIdentifierToken name, 
@@ -56,6 +73,7 @@ public class TRModule extends TRNode
 		this.files = files;
 
 		this.imports = new TreeSet<String>();
+		this.defsToHide = new HashSet<TCDefinition>();
 		this.allDefs = new TRDefinitionList();
 	}
 
@@ -63,6 +81,7 @@ public class TRModule extends TRNode
 	public void setup()
 	{
 		super.setup();
+		setFormattingSeparator("\n");
 		if (files == null || files.isEmpty())
 		{
 			// will take the module name anyhow
@@ -88,12 +107,22 @@ public class TRModule extends TRNode
 				warning(IsaWarningMessage.ISA_INVALID_MODULE_NAME_2P, name.toString(), files.toString());
 			}
 		}
+		// figure out imports first so as to add any imported definition abbreviations at the top. 
 		figureOutModuleImports();
 		allDefs.addAll(definitions);
+		
+		// after adding definitions, figure out exports as complement of definitions for adding hide_const 
+		figureOutModuleExports();
+
 		TRNode.setup(allDefs);
 		//System.out.println(toString());
 	}
 
+	/**
+	 * VDM module imports are processed by "abbreviating" them in Isabelle, which will be processed through
+	 * either TRImportedDefinition or TRRenamedDefinition. For the actual theory imports, figure out the names
+	 * of imported modules to add to the Isabelle theory import list.
+	 */
 	private void figureOutModuleImports()
 	{
 		this.imports.add(IsaToken.VDMTOOLKIT.toString());
@@ -118,12 +147,155 @@ public class TRModule extends TRNode
 		}
 	}
 
+	private Set<TCDefinition> figureOutFlatDefinitions(TCDefinitionList defsOfInterest)
+	{
+		Set<TCDefinition> result = new HashSet<TCDefinition>();
+		for(TCDefinition d : defsOfInterest)
+		{
+			// value definitions potentially have inner definitions (e.g. [A,B] = [1,2] has two definitions)
+			if (d instanceof TCValueDefinition)
+			{
+				TCValueDefinition vd = (TCValueDefinition)d;
+				result.addAll(figureOutFlatDefinitions(vd.getDefinitions()));
+			}
+			// type definitions need to consider any declared specification (e.g. inv/eq/ord)
+			else if (d instanceof TCTypeDefinition)
+			{
+				TCTypeDefinition td = (TCTypeDefinition)d;
+				result.add(td);
+
+				TCDefinitionList tdl = new TCDefinitionList();
+				if (td.invdef != null) 
+					tdl.add(td.invdef);
+				if (td.eqdef != null)
+					tdl.add(td.eqdef);
+				if (td.orddef != null)
+					tdl.add(td.orddef);
+				if (!tdl.isEmpty())
+					result.addAll(figureOutFlatDefinitions(tdl));
+			}
+			// function definitions need to consider any declared specification (e.g. pre/post/measure)
+			else if (d instanceof TCExplicitFunctionDefinition)
+			{
+				TCExplicitFunctionDefinition efd = (TCExplicitFunctionDefinition)d;
+				result.add(efd);
+
+				TCDefinitionList efdl = new TCDefinitionList();
+				if (efd.predef != null)
+					efdl.add(efd.predef);
+				if (efd.postdef != null)
+					efdl.add(efd.postdef);
+				//Measures are never translated?
+				//if (efd.measureDef != null)
+				//	efdl.add(efd.measureDef);
+				if (!efdl.isEmpty())
+					result.addAll(figureOutFlatDefinitions(efdl));
+			}
+			else if (d instanceof TCImplicitFunctionDefinition)
+			{
+				TCImplicitFunctionDefinition ifd = (TCImplicitFunctionDefinition)d;
+				result.add(ifd);
+
+				TCDefinitionList ifdl = new TCDefinitionList();
+				if (ifd.predef != null)
+					ifdl.add(ifd.predef);
+				if (ifd.postdef != null)
+					ifdl.add(ifd.postdef);
+				//if (ifd.measureDef != null)
+				//	ifdl.add(ifd.measureDef);
+				if (!ifdl.isEmpty())
+					result.addAll(figureOutFlatDefinitions(ifdl));
+			}
+			else if (d instanceof TCExplicitOperationDefinition)
+			{
+				TCExplicitOperationDefinition eod = (TCExplicitOperationDefinition)d;
+				result.add(eod);
+
+				TCDefinitionList eodl = new TCDefinitionList();
+				if (eod.predef != null)
+					eodl.add(eod.predef);
+				if (eod.postdef != null)
+					eodl.add(eod.postdef);
+				if (!eodl.isEmpty())
+					result.addAll(figureOutFlatDefinitions(eodl));
+			}
+			else if (d instanceof TCImplicitOperationDefinition)
+			{
+				TCImplicitOperationDefinition eid = (TCImplicitOperationDefinition)d;
+				result.add(eid);
+
+				TCDefinitionList eidl = new TCDefinitionList();
+				if (eid.predef != null)
+					eidl.add(eid.predef);
+				if (eid.postdef != null)
+					eidl.add(eid.postdef);
+				if (!eidl.isEmpty())
+					result.addAll(figureOutFlatDefinitions(eidl));
+			}
+			// renamed, qualified, local definitions are leafs
+			else if (d instanceof TCRenamedDefinition || d instanceof TCQualifiedDefinition || d instanceof TCLocalDefinition)
+			{
+				result.add(d);
+			}
+			// invalid TLD otherwise 
+			else 
+			{
+				report(IsaErrorMessage.VDMSL_INVALID_TLD_1P, d.toString());
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * VDM module exports are processed by "hiding" in Isabelle what is not exported
+	 * (i.e. hide all defs in "module.definitions \ module.exports"). 
+	 */
+	private void figureOutModuleExports()
+	{
+		//@NB addAll generates NPE because of TCValueDef null names!
+		//TCDefinitionSet ds = new TCDefinitionSet();
+		//ds.addAll(module.defs);
+
+		// figure out all definitions, including implicit specification etc. 
+		defsToHide.addAll(figureOutFlatDefinitions(module.defs));
+		if (module.exports != null && module.exports.exports != null)
+		{
+			for(TCExport e : module.exports.exports)
+			{
+				// if exporting all, there is nothing to hide
+				if (e instanceof TCExportAll)
+				{
+					defsToHide.clear();
+					break;
+				}
+				// with flattened out definitions set, look up/remove whatever is exported from it. 
+				else 
+				{
+					defsToHide.removeAll(e.getDefinition(module.defs));
+				}
+				// if exporting a value, the definition "mathing" is between TCLocalDef x TCValueDef, so has to be done manually
+				// else if (e instanceof TCExportedValue)
+				// {	
+				// 	TCDefinitionList dl = e.getDefinition(module.defs);
+				// 	defsToHide.removeAll(dl);
+				// }
+				// // function, operation, type; just lookup the name
+				// else 
+				// {
+				// 	defsToHide.removeAll(e.getDefinition(module.defs));
+				// }
+			}
+		}
+	}
+
 	@Override 
 	public String toString()
 	{
 		return  "Module " + name.toString() + 
 		 	"\n\t imports = " + (module != null ? module.imports != null ? module.imports.toString() : "" : "") +
-		 	"\n\t impdefs = " + (module != null ? module.importdefs != null ? module.importdefs.toString() : "" : "");
+		 	"\n\t impdefs = " + (module != null ? module.importdefs != null ? module.importdefs.toString() : "" : "") +
+		 	"\n\t exports = " + (module != null ? module.exports != null ? module.exports.toString() : "" : "") +
+		 	"\n\t expdefs = " + (module != null ? module.exportdefs != null ? module.exportdefs.toString() : "" : "");
 	}
 
 	@Override
@@ -142,19 +314,37 @@ public class TRModule extends TRNode
 		StringBuilder sb = new StringBuilder();
 		for(String i : imports)
 		{
-			sb.append(requiresQuotes(i) ? IsaToken.bracketit(IsaToken.ISAQUOTE, i, IsaToken.ISAQUOTE) : i);
+			sb.append(IsaToken.innerSyntaxIt(i));//requiresQuotes(i) ? IsaToken.innerSyntaxIt(i) : i);
 			sb.append(IsaToken.SPACE.toString());
 		}
 		return sb.toString();
 	}
 
-	private final boolean requiresQuotes(String importName)
+	public String getExports()
 	{
-		return importName.indexOf(File.pathSeparator) != -1 || 
-				importName.indexOf(File.separator) != -1 || 
-				!importName.equals(IsaToken.VDMTOOLKIT.toString()) ||
-				!IsaToken.validIsaIdentifier(importName); 
+		StringBuilder sb = new StringBuilder();
+		if (!defsToHide.isEmpty())
+		{
+			sb.append(getFormattingSeparator());
+			sb.append(IsaToken.comment(IsaInfoMessage.ISA_PROCESS_VDM_EXPORTS.message, getFormattingSeparator()));
+			for(TCDefinition d : defsToHide)
+			{
+				sb.append(d instanceof TCTypeDefinition ? IsaToken.HIDE_TYPE.toString() : IsaToken.HIDE_CONST.toString());
+				sb.append(IsaToken.SPACE.toString());
+				sb.append(IsaToken.innerSyntaxIt(IsaToken.isabelleName(d.name)));
+				sb.append(getFormattingSeparator());
+			}
+		}
+		return sb.toString();
 	}
+
+	// private final boolean requiresQuotes(String importName)
+	// {
+	// 	return importName.indexOf(File.pathSeparator) != -1 || 
+	// 			importName.indexOf(File.separator) != -1 || 
+	// 			!importName.equals(IsaToken.VDMTOOLKIT.toString()) ||
+	// 			!IsaToken.validIsaIdentifier(importName); 
+	// }
 
 	@Override
 	public String translate()
@@ -164,7 +354,10 @@ public class TRModule extends TRNode
 		{ 
 			loc += "\nfiles = " + files.toString();
 		}
-		return IsaTemplates.translateModule("", loc, name.toString(), getImports(), allDefs.translate());
+		StringBuilder sb = new StringBuilder();
+		sb.append(allDefs.translate());
+		sb.append(getExports());
+		return IsaTemplates.translateModule("", loc, name.toString(), getImports(), sb.toString());
 	}
 
     /**
