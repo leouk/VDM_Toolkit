c1b434f2d94d5122f11aa8dd922a682be0f17f28
nick_battle <nick.battle@gmail.com>
2022-02-25 14:51:18 +0000

    Added annotations for LibTests

--- plugins/vdmlib/src/test/java/vdmlib/LibTests.java
+++ plugins/vdmlib/src/test/java/vdmlib/LibTests.java
@@ -2,15 +2,32 @@
 package vdmlib;
 
 import static org.junit.Assert.assertTrue;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.fujitsu.vdmj.Settings;
 import com.fujitsu.vdmjunit.VDMJUnitTestSL;
 
 public class LibTests extends VDMJUnitTestSL
 {
+	@BeforeClass
+	public static void before()
+	{
+		Settings.annotations = true;
+	}
+	
 	@Test
-	public void CheckAsserting() throws Exception
+	public void CheckISQ() throws Exception
 	{
 		readSpecification("ISQ.vdmsl", "MATH.vdmsl", "Support1.vdmsl", "VDMUtil.vdmsl");
+		assertTrue(getWarnings().isEmpty());
+	}
+
+	@Test
+	public void CheckSupport1() throws Exception
+	{
+		readSpecification("Support1.vdmsl", "MATH.vdmsl", "VDMUtil.vdmsl");
 		assertTrue(getWarnings().isEmpty());
 	}
 }
