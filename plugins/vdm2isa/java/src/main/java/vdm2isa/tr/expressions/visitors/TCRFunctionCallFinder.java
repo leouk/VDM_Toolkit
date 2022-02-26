542eae3edc35f15bafeacb1dc5a6d6d7bf7e8024
Leo Freitas <leo.freitas@newcastle.ac.uk>
2022-02-26 10:04:45 +0100

    tolerate inv checks missing if linient is true and subtyping is the case

--- plugins/vdm2isa/java/src/main/java/vdm2isa/tr/expressions/visitors/TCRFunctionCallFinder.java
+++ plugins/vdm2isa/java/src/main/java/vdm2isa/tr/expressions/visitors/TCRFunctionCallFinder.java
@@ -20,33 +20,65 @@ import vdm2isa.tr.types.TRTypeComparator;
  */
 public class TCRFunctionCallFinder extends TCFunctionCallFinder {
 
-    public TCRFunctionCallFinder()
+    private final boolean linientInvCheck;
+
+    public TCRFunctionCallFinder(boolean linientInvCheck)
     {
         super();
+        this.linientInvCheck = linientInvCheck;
     }
 
 	@Override
 	public TCNameList caseApplyExpression(TCApplyExpression node, Object arg)
 	{
         TCNameList result = super.caseApplyExpression(node, arg);
-        // map calls don't need to be considered given they don't have pre/post conditions
-        //TODO lambda term/typed calls? They don't have pre/post either.  
-        TRType type = (TRType)TRTypeComparator.classMap(node.type.location, node.type);
-        if (node.root instanceof TCVariableExpression && type.ultimateType() instanceof TRMapType)//(node.type instanceof TCMapType))
+        if (node.root instanceof TCVariableExpression)//&& node.type instanceof TCMapType))
         {
             TCVariableExpression vexp = (TCVariableExpression)node.root;
-			result.remove(vexp.name);        
-        }
+            
+            // map calls don't need to be considered given they don't have pre/post conditions
+            //TODO lambda term/typed calls? They don't have pre/post either.  
+            TRType type = (TRType)TRTypeComparator.classMap(node.type.location, node.type);
+            if (type.ultimateType() instanceof TRMapType)
+			{
+                // remove the call if a map
+                result.remove(vexp.name);  
+                //TODO warn about being inside the domain?       
+            }
+            // linient invariant checks allow compatible/equal/narrowest types
+            // to not be checked. Concretely, whether the expression type is 
+            // a subtype of the declared function call parameter type
+            else if (linientInvCheck)
+            {
+                assert node.argtypes.size() == node.args.size();
+                Iterator<TCExpression> eit = node.args.iterator();
+                Iterator<TCType> tit = node.argtypes.iterator();
+                // presume we need to remove
+                boolean remove = true;
 
-        assert node.argtypes.size() == node.args.size();
-        Iterator<TCExpression> eit = node.args.iterator();
-        Iterator<TCType> tit = node.argtypes.iterator();
-        while (eit.hasNext())
-        {
-            TCExpression e = eit.next();
-            TCType t = tit.next();
-            //TODO when to remove the check from the result? 
-            boolean b = TypeComparator.compatible(t, e.getType());
+                // stop when found no need to remove (i.e. incompatible subtypes)
+                // or when checked every expression argument
+                while (eit.hasNext() && remove)
+                {
+                    TCExpression e = eit.next();
+                    TCType t = tit.next();
+                    // f(x+x) will not have the invariant type check for the type of x!
+                    // so only variable expressions are considered for subtyping
+                    // or apply expressions (map or function)
+                    remove =   
+                        (
+                        //  (e instanceof TCVariableExpression || 
+                        //  (e instanceof TCApplyExpression && 
+                        //((TCApplyExpression)e).root instanceof TCVariableExpression))   
+                        //&& 
+                        TypeComparator.isSubType(e.getType(), t));
+                }    
+                if (remove)
+                {
+                    result.remove(vexp.name);    
+                }
+            }
+
         }
 
         return result;
