043cbd10933088d557931447761a1ac1c6ff988c
Leo Freitas <leo.freitas@newcastle.ac.uk>
2022-02-27 12:07:39 +0000

    all traffic POs proved :-)

--- plugins/vdm2isa/java/src/test/resources/Examples/Traffic/traffic.thy
+++ plugins/vdm2isa/java/src/test/resources/Examples/Traffic/traffic.thy
@@ -1,4 +1,4 @@
-(* VDM to Isabelle Translation @2022-02-27T08:45:31.984270Z
+(* VDM to Isabelle Translation @2022-02-27T11:35:55.311633Z
    Copyright 2021, Leo Freitas, leo.freitas@newcastle.ac.uk
 
 in './src/test/resources/Examples/Traffic/traffic.vdmsl' at line 1:8
