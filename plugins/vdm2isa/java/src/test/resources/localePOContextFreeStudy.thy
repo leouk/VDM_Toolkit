1317c0c46fca88a9c9ea74e831ca5839dbf8a4a7
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2022-03-01 15:02:52 +0000

    whilst NB figures out the POG context, here is a context free locale version of the POs. USeful for multiple proofs + checking you *must* do them all ?

---
+++ plugins/vdm2isa/java/src/test/resources/localePOContextFreeStudy.thy
@@ -0,0 +1,60 @@
+theory localePOContextFreeStudy
+  imports VDMToolkit
+begin
+
+type_synonym T = VDMNat 
+
+definition 
+  inv_T :: "VDMNat \<Rightarrow> \<bool>"
+  where
+  "inv_T t \<equiv> inv_VDMNat t \<and> t \<ge> 10"
+
+lemmas inv_T_defs = inv_T_def inv_VDMNat_def
+
+definition 
+  pre_f :: "T \<Rightarrow> \<bool>"
+  where
+  "pre_f t \<equiv> inv_T t"
+
+definition 
+  post_f :: "T \<Rightarrow> VDMNat1 \<Rightarrow> \<bool>"
+  where
+  "post_f t RESULT \<equiv> inv_T t \<and> inv_VDMNat1 RESULT"
+
+lemmas pre_f_defs = pre_f_def inv_T_defs 
+lemmas post_f_defs = post_f_def inv_T_defs inv_VDMNat1_def
+
+definition 
+  f :: "T \<Rightarrow> VDMNat1"
+  where
+  "f x \<equiv> x"
+
+definition 
+  PO1 :: "\<bool>"
+  where
+  "PO1 \<equiv> (\<forall> t . inv_T t \<and> pre_f t \<longrightarrow> post_f t (f t))"
+
+lemmas PO1_defs = PO1_def inv_T_defs pre_f_defs post_f_defs
+
+definition 
+  PO2 :: "\<bool>"
+  where
+  "PO2 \<equiv> (\<exists> t . inv_T t)"
+
+lemmas PO2_defs = PO2_def inv_T_defs 
+
+locale fPOS = 
+  assumes a1: PO1
+  and a2: PO2
+
+interpretation fPOS
+  apply (unfold_locales)
+  unfolding PO1_defs
+   apply (simp add: f_def)
+  unfolding PO2_defs
+  by presburger
+
+interpretation fPOS
+  apply (unfold_locales)
+  oops
+end
