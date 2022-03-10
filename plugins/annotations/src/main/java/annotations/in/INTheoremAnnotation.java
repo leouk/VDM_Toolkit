344721a5fe725cf50e7ca1a22dc5d8a1dcaf19e5
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2022-03-10 13:19:59 +0000

    proof by symbolic execution if possible.

---
+++ plugins/annotations/src/main/java/annotations/in/INTheoremAnnotation.java
@@ -0,0 +1,39 @@
+package annotations.in;
+
+import com.fujitsu.vdmj.in.annotations.INAnnotation;
+import com.fujitsu.vdmj.in.expressions.INExpression;
+import com.fujitsu.vdmj.in.expressions.INExpressionList;
+import com.fujitsu.vdmj.messages.Console;
+import com.fujitsu.vdmj.runtime.Context;
+import com.fujitsu.vdmj.runtime.ContextException;
+import com.fujitsu.vdmj.tc.lex.TCIdentifierToken;
+import com.fujitsu.vdmj.values.Value;
+
+public class INTheoremAnnotation extends INAnnotation {
+
+    public INTheoremAnnotation(TCIdentifierToken name, INExpressionList args) {
+        super(name, args);
+    }
+
+    @Override
+    protected void doInit(Context ctxt)
+    {
+        INExpression e = args.get(0);
+        Console.out.print("Attempting proof by symbolic execution for @Theorem " + e.toString() + 
+            " in " + e.getLocation().module + " " + args.get(0).getLocation().toShortString());
+        for(int i = 1; i < args.size(); i++)
+        {
+            try 
+            {
+                Value v = args.get(i).eval(ctxt);
+                Console.out.println("\t= " + v.toString());
+            }
+            catch (ContextException t)
+            {
+                // not executable
+                Console.out.println("\t= theorem is not executable");
+            }
+        }
+
+    }
+}
