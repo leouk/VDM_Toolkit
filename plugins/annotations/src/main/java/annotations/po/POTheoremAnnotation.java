7153698bcf041d0b0268850eaa3944e7b4acc57b
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2022-03-10 13:34:54 +0000

    added to POG, but still needs to fix the PONameContext to be "nicer" output. But all there now then :-). Cool.

--- plugins/annotations/src/main/java/annotations/po/POTheoremAnnotation.java
+++ plugins/annotations/src/main/java/annotations/po/POTheoremAnnotation.java
@@ -1,12 +1,18 @@
 package annotations.po;
 
+import java.util.Arrays;
+
 import com.fujitsu.vdmj.po.annotations.POAnnotation;
+import com.fujitsu.vdmj.po.definitions.PODefinition;
 import com.fujitsu.vdmj.po.expressions.POExpressionList;
+import com.fujitsu.vdmj.po.expressions.POVariableExpression;
 import com.fujitsu.vdmj.po.modules.POModule;
 import com.fujitsu.vdmj.pog.POContextStack;
+import com.fujitsu.vdmj.pog.PONameContext;
 import com.fujitsu.vdmj.pog.ProofObligationList;
 import com.fujitsu.vdmj.pog.TheoremObligation;
 import com.fujitsu.vdmj.tc.lex.TCIdentifierToken;
+import com.fujitsu.vdmj.tc.lex.TCNameList;
 
 public class POTheoremAnnotation extends POAnnotation {
 
@@ -17,11 +23,15 @@ public class POTheoremAnnotation extends POAnnotation {
     }
 
     @Override 
-    public void poAfter(POModule module, ProofObligationList list)
+    public void poAfter(PODefinition def, ProofObligationList obligations, POContextStack ctxt)
 	{
-		TheoremObligation po = new TheoremObligation(module.location, args.get(0).toString(), args.get(1).toString(), new POContextStack());
-        list.add(po);
-	}
+        //@NB TheoremObligation doesn't make use of the name argument? Does it need to go into ctxt?
+        //    From the output this is not quite right, but wasn't sure the way forward on this one. 
+        assert args.get(0) instanceof POVariableExpression;
+        POVariableExpression poName = (POVariableExpression)args.get(0);
+        ctxt.push(new PONameContext(new TCNameList(poName.name)));
 
-    
+		TheoremObligation po = new TheoremObligation(def.location, poName.name.toString(), args.get(1).toString(), ctxt);
+        obligations.add(po);  
+	}
 }
