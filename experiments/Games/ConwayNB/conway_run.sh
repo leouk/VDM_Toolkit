01b522218287c330654ba04c44800c2d28c550a1
Leo Freitas <leo.freitas@newcastle.ac.uk>
2022-03-11 11:05:48 +0000

    move examples around

--- experiments/Games/ConwayNB/conway_run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rlwrap java -cp ~/.m2/repository/com/fujitsu/vdmj/4.4.4-SNAPSHOT/vdmj-4.4.4-SNAPSHOT.jar:bin com.fujitsu.vdmj.VDMJ -vdmsl -remote Conway Conway.vdmsl
--- experiments/Heap/scripts/heap-typecheck.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo $1
-java -Xmx2048m -jar "/Users/nljsf/Documents/research/java/OvertureIde-1.0.0[May]/plugins/org.overture.ide.generated.vdmj_0.3.2/lib/vdmj-2.0.2.jar" -vdmsl -r vdm10 ./Heap4.vdmsl ./Heap3.vdmsl ./Heap2.vdmsl ./Heap1.vdmsl ./Heap0.vdmsl  ./lib/IO.vdmsl ./lib/VDMUtil.vdmsl
