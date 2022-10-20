bebcb5f4277dbd1c48920dde560af87e08a307fd
Leo Freitas <leo.freitas@newcastle.ac.uk>
2022-10-20 14:40:28 +0100

    set link aware of separate VDMJ and VDMToolkit versions

--- scripts/setlinks.sh
+++ scripts/setlinks.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 # Copy jars and set symlinks to Maven
 
-if [ $# -ne 2 ]
+if [ $# -ne 3 ]
 then
-    echo "setlinks.sh <VDM VSCode version> <VDMJ suite version>"
+    echo "setlinks.sh <VDM VSCode version> <VDMJ VDMJSUITE version> <VDM Toolkit VDMJSUITE version>"
     exit 1
 fi
 
 cd $HOME/.vscode
 VERSION=$1
-SUITE=$2
+VDMJSUITE=$2
+VDMTKSUITE=$3
+echo "Set links for VDM VSCode $VERSION, VDMJ $VDMJSUITE, VDMToolkit $VDMTKSUITE"
 
 if [ ! -e extensions/overturetool.vdm-vscode-$VERSION ]
 then
@@ -33,30 +35,31 @@ do
 	rm $RESOURCES/$DIR/vdm2isa*.jar 
     rm $RESOURCES/$DIR/libs/stdlib*.jar
 	rm $RESOURCES/$DIR/libs/vdmlib*.jar 
-    echo "Cleaned $DIR"
+    echo "Cleaned $RESOURCES/$DIR"
 done
 
-ln -sf $HOME/.m2/repository/$GROUPID/annotations/$SUITE$SNAPSHOT/annotations-$SUITE$SNAPSHOT.jar $RESOURCES/jars/vdmj/annotations-$SUITE$SNAPSHOT.jar
-echo "Created annotations from $HOME/.m2/repository/$GROUPID/annotations/$SUITE$SNAPSHOT/annotations-$SUITE$SNAPSHOT.jar to $RESOURCES/jars/vdmj/annotations-$SUITE$SNAPSHOT.jar"
-ln -sf $HOME/.m2/repository/$GROUPID/annotations2/$SUITE$SNAPSHOT/annotations2-$SUITE$SNAPSHOT.jar $RESOURCES/jars/vdmj/annotations2-$SUITE$SNAPSHOT.jar
-ln -sf $HOME/.m2/repository/$GROUPID/vdmj/$SUITE$SNAPSHOT/vdmj-$SUITE$SNAPSHOT.jar $RESOURCES/jars/vdmj/vdmj-$SUITE$SNAPSHOT.jar
-ln -sf $HOME/.m2/repository/$GROUPID/lsp/$SUITE$SNAPSHOT/lsp-$SUITE$SNAPSHOT.jar $RESOURCES/jars/vdmj/lsp-$SUITE$SNAPSHOT.jar
-ln -sf $HOME/.m2/repository/$GROUPID/stdlib/$SUITE$SNAPSHOT/stdlib-$SUITE$SNAPSHOT.jar $RESOURCES/jars/vdmj/libs/stdlib-$SUITE$SNAPSHOT.jar
+ln -sf $HOME/.m2/repository/$GROUPID/annotations/$VDMJSUITE-SNAPSHOT/annotations-$VDMJSUITE-SNAPSHOT.jar $RESOURCES/jars/vdmj 	#$RESOURCES/jars/vdmj/annotations-$VDMJSUITE-SNAPSHOT.jar
+echo "Created annotations from $HOME/.m2/repository/$GROUPID/annotations/$VDMJSUITE-SNAPSHOT/annotations-$VDMJSUITE-SNAPSHOT.jar to $RESOURCES/jars/vdmj/annotations-$VDMJSUITE-SNAPSHOT.jar"
+ln -sf $HOME/.m2/repository/$GROUPID/annotations2/$VDMJSUITE-SNAPSHOT/annotations2-$VDMJSUITE-SNAPSHOT.jar $RESOURCES/jars/vdmj # $RESOURCES/jars/vdmj/annotations2-$VDMJSUITE-SNAPSHOT.jar
+ln -sf $HOME/.m2/repository/$GROUPID/vdmj/$VDMJSUITE-SNAPSHOT/vdmj-$VDMJSUITE-SNAPSHOT.jar $RESOURCES/jars/vdmj 				#$RESOURCES/jars/vdmj/vdmj-$VDMJSUITE-SNAPSHOT.jar
+ln -sf $HOME/.m2/repository/$GROUPID/lsp/$VDMJSUITE-SNAPSHOT/lsp-$VDMJSUITE-SNAPSHOT.jar $RESOURCES/jars/vdmj 					#$RESOURCES/jars/vdmj/lsp-$VDMJSUITE-SNAPSHOT.jar
+ln -sf $HOME/.m2/repository/$GROUPID/stdlib/$VDMJSUITE-SNAPSHOT/stdlib-$VDMJSUITE-SNAPSHOT.jar $RESOURCES/jars/vdmj/libs 		#$RESOURCES/jars/vdmj/libs/stdlib-$VDMJSUITE-SNAPSHOT.jar
 echo "Created jars/vdmj links"
 
-ln -sf $HOME/.m2/repository/$GROUPID/annotations/$SUITE-P$SNAPSHOT/annotations-$SUITE-P$SNAPSHOT.jar $RESOURCES/jars/vdmj_hp
-ln -sf $HOME/.m2/repository/$GROUPID/annotations2/$SUITE-P$SNAPSHOT/annotations2-$SUITE-P$SNAPSHOT.jar $RESOURCES/jars/vdmj_hp
-ln -sf $HOME/.m2/repository/$GROUPID/vdmj/$SUITE-P$SNAPSHOT/vdmj-$SUITE-P$SNAPSHOT.jar $RESOURCES/jars/vdmj_hp
-ln -sf $HOME/.m2/repository/$GROUPID/lsp/$SUITE-P$SNAPSHOT/lsp-$SUITE-P$SNAPSHOT.jar $RESOURCES/jars/vdmj_hp
-ln -sf $HOME/.m2/repository/$GROUPID/stdlib/$SUITE-P$SNAPSHOT/stdlib-$SUITE-P$SNAPSHOT.jar $RESOURCES/jars/vdmj_hp/libs
+ln -sf $HOME/.m2/repository/$GROUPID/annotations/$VDMJSUITE-P-SNAPSHOT/annotations-$VDMJSUITE-P-SNAPSHOT.jar $RESOURCES/jars/vdmj_hp
+ln -sf $HOME/.m2/repository/$GROUPID/annotations2/$VDMJSUITE-P-SNAPSHOT/annotations2-$VDMJSUITE-P-SNAPSHOT.jar $RESOURCES/jars/vdmj_hp
+ln -sf $HOME/.m2/repository/$GROUPID/vdmj/$VDMJSUITE-P-SNAPSHOT/vdmj-$VDMJSUITE-P-SNAPSHOT.jar $RESOURCES/jars/vdmj_hp
+ln -sf $HOME/.m2/repository/$GROUPID/lsp/$VDMJSUITE-P-SNAPSHOT/lsp-$VDMJSUITE-P-SNAPSHOT.jar $RESOURCES/jars/vdmj_hp
+ln -sf $HOME/.m2/repository/$GROUPID/stdlib/$VDMJSUITE-P-SNAPSHOT/stdlib-$VDMJSUITE-P-SNAPSHOT.jar $RESOURCES/jars/vdmj_hp/libs
 echo "Created jars/vdmj_hp links"
 
-ln -sf $HOME/.m2/repository/$VDMTOOLKIT/annotationsVDMToolkit/$SUITE-SNAPSHOT/annotationsVDMToolkit-$SUITE$SNAPSHOT.jar $RESOURCES/jars/vdmj
-ln -sf $HOME/.m2/repository/$VDMTOOLKIT/vdm2isa/$SUITE$SNAPSHOT/vdm2isa-$SUITE$SNAPSHOT.jar $RESOURCES/jars/plugins
-ln -sf $HOME/.m2/repository/$VDMTOOLKIT/vdmlib/$SUITE$SNAPSHOT/vdmlib-$SUITE$SNAPSHOT.jar $RESOURCES/jars/vdmj/libs
+ln -sf $HOME/.m2/repository/$VDMTOOLKIT/annotationsVDMToolkit/$VDMTKSUITE-SNAPSHOT/annotationsVDMToolkit-$VDMTKSUITE-SNAPSHOT.jar $RESOURCES/jars/vdmj
+ln -sf $HOME/.m2/repository/$VDMTOOLKIT/vdm2isa/$VDMTKSUITE-SNAPSHOT/vdm2isa-$VDMTKSUITE-SNAPSHOT.jar $RESOURCES/jars/plugins
+ln -sf $HOME/.m2/repository/$VDMTOOLKIT/vdm2isa-lsp/$VDMTKSUITE-SNAPSHOT/vdm2isa-lsp-$VDMTKSUITE-SNAPSHOT.jar $RESOURCES/jars/plugins
+ln -sf $HOME/.m2/repository/$VDMTOOLKIT/vdmlib/$VDMTKSUITE-SNAPSHOT/vdmlib-$VDMTKSUITE-SNAPSHOT.jar $RESOURCES/jars/vdmj/libs
 echo "Created jars/vdmtoolkit links"
 
-ln -sf $HOME/.m2/repository/$VDMTOOLKIT/vdmlib/$SUITE$SNAPSHOT/vdmlib-$SUITE-P$SNAPSHOT.jar $RESOURCES/jars/vdmj_hp/libs
+ln -sf $HOME/.m2/repository/$VDMTOOLKIT/vdmlib/$VDMTKSUITE-SNAPSHOT/vdmlib-$VDMTKSUITE-P-SNAPSHOT.jar $RESOURCES/jars/vdmj_hp/libs
 echo "Created high precision vdmtoolkit vdmlib links"
 
 echo "Done"
