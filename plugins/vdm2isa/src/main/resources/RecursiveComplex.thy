65b8bd0fac514626ff62769b4f375a87366bf75b
Leo Freitas <leo.freitas@newcastle.ac.uk>
2022-11-04 16:15:22 +0000

    various lemmas about corresponing well foundedness of the type int, to be used for complex recursive functions for VDMNat (int).

--- plugins/vdm2isa/src/main/resources/RecursiveComplex.thy
+++ plugins/vdm2isa/src/main/resources/RecursiveComplex.thy
@@ -59,6 +59,106 @@ function (domintros)
   \<close>
   by (pat_completeness, auto)
 
+definition pred_VDMNat :: "(VDMNat \<times> VDMNat) set"
+  where "pred_VDMNat \<equiv> {(z, z+1) | z . inv_VDMNat z }"
+
+text \<open>Project @{term pred_VDMNat} into @{typ \<nat>} for link to well foundedness\<close>
+lemma l_pred_VDMNat_wf: \<open>wf pred_VDMNat\<close>
+proof -
+  have "pred_VDMNat \<subseteq> measure nat" 
+     by (auto simp add: pred_VDMNat_def inv_VDMNat_def)
+  then show ?thesis
+     by (rule wf_subset [OF wf_measure])
+qed
+
+lemma l_pred_VDMNat_subset_int_ge_less_than: 
+  \<open>pred_VDMNat \<subseteq> int_ge_less_than 0\<close>
+  by (simp add: int_ge_less_than_def inv_VDMNat_def pred_VDMNat_def subset_iff)
+
+definition less_than_VDMNat :: "(VDMNat \<times> VDMNat) set"
+(*  where "less_than_VDMNat \<equiv> pred_VDMNat\<^sup>+"*)
+  where \<open>less_than_VDMNat \<equiv> (int_ge_less_than 0)\<^sup>+\<close>
+
+lemma l_less_than_VDMNat_wf [iff]: "wf less_than_VDMNat"
+  by (simp add: less_than_VDMNat_def wf_int_ge_less_than wf_trancl)
+
+lemma trans_less_than_VDMNat [iff]: "trans less_than_VDMNat"
+  by (simp add: less_than_VDMNat_def)
+
+lemma l_less_than_VDMNat_irrefl: "irrefl less_than_VDMNat"
+  by (simp add: wf_imp_irrefl)
+
+lemma l_less_than_VDMNat_asym: "asym less_than_VDMNat"
+  by (meson asym_iff l_less_than_VDMNat_wf wf_not_sym)
+
+lemma less_int_rel: "(<) = (\<lambda>m n. m < (n::int))\<^sup>+\<^sup>+"
+proof (rule ext, rule ext, rule iffI)
+  fix n m :: int
+  show "(\<lambda>m n. m < (n::int))\<^sup>+\<^sup>+ m n" if "m < n"
+    using that
+  proof (induct n)
+    case base
+    then show ?case by auto
+  next
+    case (step i)
+    then show ?case 
+      by (auto intro: tranclp.trancl_into_trancl)
+  qed
+  show "m < n" if "(\<lambda>m n. m < (n::int))\<^sup>+\<^sup>+ m n"
+    using that by (induct n) (simp_all)
+qed
+
+lemma l_less_than_VDMNat_subset_int_ge_less_than:
+  "less_than_VDMNat \<subseteq> int_ge_less_than 0"
+  apply (simp add: less_than_VDMNat_def int_ge_less_than_def inv_VDMNat_def  pred_VDMNat_def subset_iff)
+  apply (intro allI impI conjI)
+  using tranclD apply fastforce 
+  by (smt (z3) case_prodD mem_Collect_eq trancl_trans_induct)
+
+lemma l_less_than_VDMNat_iff [iff]: "inv_VDMNat x \<Longrightarrow> ((x,y) \<in> less_than_VDMNat) = (x<y)"
+  apply (intro iffI)  
+  using int_ge_less_than_def l_less_than_VDMNat_subset_int_ge_less_than apply fastforce
+  unfolding less_than_VDMNat_def int_ge_less_than_def 
+  by (rule r_into_trancl, simp add: inv_VDMNat_def)
+
+lemma l_total_on_less_than_VDMNat [simp]: "total_on { x . inv_VDMNat x} less_than_VDMNat"
+  unfolding total_on_def 
+  using inv_VDMNat_def by fastforce
+
+definition
+  pair_less_VDMNat :: \<open>((VDMNat \<times> VDMNat) \<times> (VDMNat \<times> VDMNat)) VDMSet\<close>
+  where
+  "pair_less_VDMNat \<equiv> less_than_VDMNat <*lex*> less_than_VDMNat"
+
+lemma wf_pair_less_VDMNat[simp]: "wf pair_less_VDMNat"
+  by (auto simp: pair_less_VDMNat_def)
+
+lemma l_trans_pair_less_VDMNat [iff]: "trans pair_less_VDMNat"
+  by (simp add: pair_less_VDMNat_def)
+
+lemma total_pair_less_VDMNat [iff]: "total_on less_than_VDMNat pair_less_VDMNat" 
+  unfolding total_on_def pair_less_VDMNat_def lex_prod_def
+  apply safe
+  apply (metis (mono_tags, lifting) case_prodD int_ge_less_than_def inv_VDMNat_def l_less_than_VDMNat_iff l_less_than_VDMNat_subset_int_ge_less_than mem_Collect_eq not_less_iff_gr_or_eq subsetD)
+  apply (metis (no_types, lifting) Product_Type.Collect_case_prodD fst_conv insert_absorb insert_subset int_ge_less_than_def inv_VDMNat_def l_less_than_VDMNat_iff l_less_than_VDMNat_subset_int_ge_less_than not_less_iff_gr_or_eq)
+  apply (metis (no_types, lifting) case_prodD in_mono int_ge_less_than_def inv_VDMNat_def l_less_than_VDMNat_iff l_less_than_VDMNat_subset_int_ge_less_than mem_Collect_eq not_less_iff_gr_or_eq)
+  apply (metis (no_types, lifting) case_prodD int_ge_less_than_def inv_VDMNat_def l_less_than_VDMNat_iff l_less_than_VDMNat_subset_int_ge_less_than mem_Collect_eq not_less_iff_gr_or_eq subsetD)
+  apply (metis (mono_tags, lifting) case_prodD in_mono int_ge_less_than_def inv_VDMNat_def l_less_than_VDMNat_iff l_less_than_VDMNat_subset_int_ge_less_than mem_Collect_eq not_less_iff_gr_or_eq)
+  apply (metis (no_types, lifting) case_prodD int_ge_less_than_def inv_VDMNat_def l_less_than_VDMNat_iff l_less_than_VDMNat_subset_int_ge_less_than mem_Collect_eq not_less_iff_gr_or_eq subsetD)
+  apply (metis (no_types, lifting) case_prodD in_mono int_ge_less_than_def inv_VDMNat_def l_less_than_VDMNat_iff l_less_than_VDMNat_subset_int_ge_less_than mem_Collect_eq not_less_iff_gr_or_eq)
+  by (smt (verit, best) Product_Type.Collect_case_prodD fst_conv int_ge_less_than_def inv_VDMNat_def l_less_than_VDMNat_iff l_less_than_VDMNat_subset_int_ge_less_than subset_iff)
+
+lemma l_pair_less_VDMNat_iff1 [simp]: "inv_VDMNat x \<Longrightarrow> ((x,y), (x,z)) \<in> pair_less_VDMNat \<longleftrightarrow> y<z"
+  apply (intro iffI) 
+  sledgehammer
+  unfolding less_than_VDMNat_def int_ge_less_than_def 
+  by (rule r_into_trancl, simp add: inv_VDMNat_def)
+
+lemma l_pair_less_VDMNat_I1: "inv_VDMNat a \<Longrightarrow> a < b  \<Longrightarrow> ((a, s), (b, t)) \<in> pair_less_VDMNat"
+  sorry
+lemma l_pair_less_VDMNat_I2: "inv_VDAMNat a \<Longrightarrow> a \<le> b \<Longrightarrow> s < t \<Longrightarrow> ((a, s), (b, t)) \<in> pair_less_VDMNat"
+  sorry
+
 definition 
   ack_wf_rel :: \<open>((VDMNat \<times> VDMNat) \<times> (VDMNat \<times> VDMNat)) VDMSet\<close>
   where
@@ -70,8 +170,9 @@ definition
       { ((m-1, n), (m, n)) | m n . m \<noteq> 0 \<and> n \<noteq> 0 \<and> pre_ack m n } 
   \<close>   (* how to represent the last call site? Is this okay? *)
 
-lemma l_ack_wf_rel[simp]: "wf ack_wf_rel" sorry
+lemma l_ack_wf_rel[simp]: "wf ack_wf_rel" oops
 termination
+  apply (relation pair_less)
   apply (rule "termination"[of \<open>ack_wf_rel\<close>])
      apply simp
     apply (simp add: ack_wf_rel_def)
@@ -87,9 +188,20 @@ function ack' :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
 | "ack' (Suc m) (Suc n) = ack' m (ack' (Suc m) n)"
         apply (pat_completeness, auto) done
 termination 
+(*
   find_theorems "wf (_ \<union> _)"
-  thm lessI pair_lessI1 pair_less_def pair_less_iff1 wf_pair_less
   using "termination" lessI pair_lessI1 pair_less_iff1 wf_pair_less by presburger
+*)
+  thm lessI pair_lessI1 pair_less_def pair_less_iff1 wf_pair_less
+  apply (relation pair_less)
+     apply (simp)
+  using lessI pair_lessI1 apply presburger
+  using lessI pair_less_iff1 apply presburger
+  using lessI pair_lessI1 by presburger
+
+theorem ack_correct: \<open>ack' m n = ack m n\<close>
+  apply (induction m n rule: ack'.induct)
+  by (simp add: pre_ack_defs)+
 
 function 
   tak :: "VDMInt \<Rightarrow> VDMInt \<Rightarrow> VDMInt \<Rightarrow> VDMInt" 
@@ -99,18 +211,13 @@ function
 
 lemma tak_pcorrect:
   "tak_dom (x, y, z) \<Longrightarrow> tak x y z = (if x \<le> y then y else if y \<le> z then z else x)"
-  by (induction x y z rule: tak.pinduct) (auto simp: tak.psimps)
+  thm tak.pinduct tak.psimps
+  apply (induction x y z rule: tak.pinduct) 
+  by (simp add: tak.psimps)
 
 definition tak_m1 where "tak_m1 = (\<lambda>(x,y,z). if x \<le> y then 0 else 1)"
 definition tak_m2 where "tak_m2 = (\<lambda>(x,y,z). nat (Max {x, y, z} - Min {x, y, z}))"
 definition tak_m3 where "tak_m3 = (\<lambda>(x,y,z). nat (x - Min {x, y, z}))"
-
-lemma "tak_m1 <*mlex*> tak_m2 <*mlex*> tak_m3 <*mlex*> {} 
-      = 
-      tak_m1 <*mlex*> (tak_m2 <*mlex*> (tak_m3 <*mlex*> {}))" by simp
-
-lemma "((x1,y1,z1),(x2,y2,z2)) \<in> tak_m3 <*mlex*> R"
-   apply (simp add: mlex_iff) oops
 
 lemma "(((x1,y1,z1),(x2,y2,z2)) \<in> tak_m3 <*mlex*> {}) \<longleftrightarrow> (nat (x1 - Min {x1, y1, z1}) < nat (x2 - Min {x2, y2, z2}))"
   apply (simp add: mlex_iff)
@@ -118,33 +225,43 @@ lemma "(((x1,y1,z1),(x2,y2,z2)) \<in> tak_m3 <*mlex*> {}) \<longleftrightarrow> 
   (*apply (simp only: case_prod_beta fst_conv snd_conv) *)
   by simp
 
-lemma \<open>\<lbrakk>x1 > y1; x2 > y2; x1 \<noteq> x2; y1 \<noteq> y2; z1 \<noteq> z2\<rbrakk> 
-       \<Longrightarrow> 
-       ((x1,y1,z1),(x2,y2,z2)) \<in> (tak_m2 <*mlex*> (tak_m3 <*mlex*> {}))\<close>
+lemma \<open>((x1,y1,z1),(x2,y2,z2)) \<in> (tak_m2 <*mlex*> (tak_m3 <*mlex*> {}))\<close>
   apply (simp add: mlex_iff)
   unfolding tak_m3_def tak_m2_def
-  apply (simp, clarify)
-  apply (simp only: case_prod_beta fst_conv snd_conv)
-  nitpick
-  apply (clarify, safe) 
-  by simp
+  apply (simp only: case_prod_beta fst_conv snd_conv, clarify)
+  oops
 
-lemma "tak_m1 <*mlex*> tak_m2 <*mlex*> tak_m3 <*mlex*> {} = A"
-  apply (intro equalityI subsetI)
-   defer using [[]]
-   apply (case_tac x, simp)
-   apply (case_tac a, simp)
+lemma l_call1: 
+  \<open>x > y \<Longrightarrow> ((x-1,y,z),(x,y,z)) \<in> tak_m1 <*mlex*> (tak_m2 <*mlex*> (tak_m3 <*mlex*> {}))\<close> 
   apply (simp add: mlex_iff)
-(*   apply (simp add: tak_m1_def tak_m2_def tak_m3_def case_prod_beta)
-  apply safe
-  thm mlex_iff wf_mlex
-  unfolding mlex_prod_def lex_prod_def tak_m1_def tak_m2_def tak_m3_def 
-  apply (simp add: case_prod_beta)*)
-  sorry
-termination
-  by (relation "tak_m1 <*mlex*> tak_m2 <*mlex*> tak_m3 <*mlex*> {}")
-     (auto simp: mlex_iff wf_mlex tak_pcorrect tak_m1_def tak_m2_def tak_m3_def min_def max_def)
+  unfolding tak_m3_def tak_m2_def tak_m1_def
+  apply (simp add: case_prod_beta)
+  unfolding min_def max_def
+  by (simp_all split:if_splits)
 
+lemma l_call2:
+  \<open>x > y \<Longrightarrow> ((y-1,z,x),(x,y,z)) \<in> tak_m1 <*mlex*> (tak_m2 <*mlex*> (tak_m3 <*mlex*> {}))\<close>
+  apply (simp add: mlex_iff)
+  unfolding tak_m3_def tak_m2_def tak_m1_def
+  by (simp add: case_prod_beta)
+
+lemma l_call3:
+  \<open>x > y \<Longrightarrow> ((z-1,x,y),(x,y,z)) \<in> tak_m1 <*mlex*> (tak_m2 <*mlex*> (tak_m3 <*mlex*> {}))\<close>
+  apply (simp add: mlex_iff)
+  unfolding tak_m3_def tak_m2_def tak_m1_def
+  by (simp add: case_prod_beta)
+
+termination
+  apply (relation "tak_m1 <*mlex*> tak_m2 <*mlex*> tak_m3 <*mlex*> {}") 
+      apply (simp add: wf_mlex)
+     apply (simp add: l_call1)
+     apply (simp add: l_call2)
+     apply (simp add: l_call3)
+  sledgehammer
+(*
+     apply
+     (auto simp: mlex_iff wf_mlex tak_pcorrect tak_m1_def tak_m2_def tak_m3_def min_def max_def)
+*)
 theorem tak_correct: "tak x y z = (if x \<le> y then y else if y \<le> z then z else x)"
   by (induction x y z rule: tak.induct) auto
 
