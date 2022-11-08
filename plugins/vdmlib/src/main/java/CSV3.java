4d5d36e79203761e88baab98a5a636f69ef89177
Leo Freitas <leo.freitas@newcastle.ac.uk>
2022-11-08 10:52:00 +0000

    initial implementation with "trivial" state machine. Might have multiple implementation choices?

--- plugins/vdmlib/src/main/java/CSV3.java
+++ plugins/vdmlib/src/main/java/CSV3.java
@@ -1,5 +1,7 @@
+import java.io.File;
 import java.io.Serializable;
 
+import com.fujitsu.vdmj.runtime.VDMFunction;
 import com.fujitsu.vdmj.runtime.VDMOperation;
 import com.fujitsu.vdmj.values.NilValue;
 import com.fujitsu.vdmj.values.SeqValue;
@@ -14,13 +16,50 @@ import com.fujitsu.vdmj.values.Value;
 public class CSV3 implements Serializable {
 
     private static final long serialVersionUID = 1L;
-	private static String lastError = "";
-    
-    @VDMOperation
-	public static Value ferror()
+    private static String lastFilePath = null;
+	private static String lastErrorStr = "";
+
+    protected static File getFile(Value path)
 	{
-		return lastError.isEmpty() ? 
+		String adjustedPath = IO.stringOf(path).replace('/', File.separatorChar);        
+		File f = new File(adjustedPath).getAbsoluteFile();
+        return f;
+	}
+
+    /**
+     * Corresponds to VDM "file_status: Path -> FileStatus"
+     * @param path
+     * @return FileStatus
+     */
+    @VDMFunction 
+    public static Value file_status(Value path)
+    {
+        File f = getFile(path);
+        f.isDirectory()
+        f.canRead()
+        return null;
+    }
+
+    /**
+     * Corresponds to VDM "csv_read[@p]: Path -> bool * [@p]"
+     * @param path full file path where it is expected file_status(path) = <Valid>
+     * @return
+     */
+    @VDMFunction
+    public static Value csv_read(Value path)
+    {
+        return null;
+    }
+
+    /**
+     * Corresponds to VDM "lastError: () ==> [String1]".
+     * @return [String1]
+     */
+    @VDMOperation
+	public static Value lastError()
+	{
+		return lastErrorStr.isEmpty() ? 
             new NilValue() : 
-            new SeqValue(lastError);
+            new SeqValue(lastErrorStr);
 	}
 }
---
+++ plugins/vdmlib/src/main/java/util/CsvParser3.java
@@ -0,0 +1,107 @@
+package util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class CsvParser3 {
+    
+    // Idea from 
+    public static Iterable<String[]> parseCSV(final InputStream stream) throws IOException {
+        return new Iterable<String[]>() {
+            @Override
+            public Iterator<String[]> iterator() {
+                return new Iterator<String[]>() {
+                    static final int UNCALCULATED = 0;
+                    static final int READY = 1;
+                    static final int FINISHED = 2;
+                    int state = UNCALCULATED;
+                    ArrayList<String> value_list = new ArrayList<>();
+                    StringBuilder sb = new StringBuilder();
+                    String[] return_value;
+    
+                    public void end() {
+                        end_part();
+                        return_value = new String[value_list.size()];
+                        value_list.toArray(return_value);
+                        value_list.clear();
+                    }
+    
+                    public void end_part() {
+                        value_list.add(sb.toString());
+                        sb.setLength(0);
+                    }
+    
+                    public void append(int ch) {
+                        sb.append((char) ch);
+                    }
+    
+                    public void calculate() throws IOException {
+                        boolean inquote = false;
+                        while (true) {
+                            int ch = stream.read();
+                            switch (ch) {
+                                default: //regular character.
+                                    append(ch);
+                                    break;
+                                case -1: //read has reached the end.
+                                    if ((sb.length() == 0) && (value_list.isEmpty())) {
+                                        state = FINISHED;
+                                    } else {
+                                        end();
+                                        state = READY;
+                                    }
+                                    return;
+                                case '\r':
+                                case '\n': //end of line.
+                                    if (inquote) {
+                                        append(ch);
+                                    } else {
+                                        end();
+                                        state = READY;
+                                        return;
+                                    }
+                                    break;
+                                case ',': //comma
+                                    if (inquote) {
+                                        append(ch);
+                                    } else {
+                                        end_part();
+                                        break;
+                                    }
+                                    break;
+                                case '"': //quote.
+                                    inquote = !inquote;
+                                    break;
+                            }
+                        }
+                    }
+    
+                    @Override
+                    public boolean hasNext() {
+                        if (state == UNCALCULATED) {
+                            try {
+                                calculate();
+                            } catch (IOException ex) {
+                            }
+                        }
+                        return state == READY;
+                    }
+    
+                    @Override
+                    public String[] next() {
+                        if (state == UNCALCULATED) {
+                            try {
+                                calculate();
+                            } catch (IOException ex) {
+                            }
+                        }
+                        state = UNCALCULATED;
+                        return return_value;
+                    }
+                };
+            }
+        };
+    }
+}
