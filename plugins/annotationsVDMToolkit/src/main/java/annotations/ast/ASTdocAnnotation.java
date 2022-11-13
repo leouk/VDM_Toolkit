16edd38543b813291d5fdf8bf32ef3c0193bbaa7
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2022-11-13 11:05:34 +0000

    removed specialised parsing from @doc annotation (for now) + added @todo annotation just for tagging for now.

--- plugins/annotationsVDMToolkit/src/main/java/annotations/ast/ASTdocAnnotation.java
+++ plugins/annotationsVDMToolkit/src/main/java/annotations/ast/ASTdocAnnotation.java
@@ -1,16 +1,7 @@
 package annotations.ast;
 
 import com.fujitsu.vdmj.ast.annotations.ASTAnnotation;
-import com.fujitsu.vdmj.ast.expressions.ASTEqualsExpression;
-import com.fujitsu.vdmj.ast.expressions.ASTExpression;
-import com.fujitsu.vdmj.ast.expressions.ASTExpressionList;
-import com.fujitsu.vdmj.ast.expressions.ASTVariableExpression;
 import com.fujitsu.vdmj.ast.lex.LexIdentifierToken;
-import com.fujitsu.vdmj.lex.LexException;
-import com.fujitsu.vdmj.lex.LexTokenReader;
-import com.fujitsu.vdmj.lex.Token;
-import com.fujitsu.vdmj.syntax.ExpressionReader;
-import com.fujitsu.vdmj.syntax.ParserException;
 
 public class ASTdocAnnotation extends ASTAnnotation {
 
@@ -18,41 +9,41 @@ public class ASTdocAnnotation extends ASTAnnotation {
         super(name);
     }
     
-    /**
-	 * Override the default parse, and look for @TheoremAttribute <name> = <exp>;
-	 */
-	@Override
-	public ASTExpressionList parse(LexTokenReader ltr) throws LexException, ParserException
-	{
-		ltr.nextToken();
-		ASTExpressionList args = new ASTExpressionList();
-		ExpressionReader er = new ExpressionReader(ltr);
-		ASTExpression exp = er.readExpression();
+    // /**
+	//  * Override the default parse, and look for @TheoremAttribute <name> = <exp>;
+	//  */
+	// @Override
+	// public ASTExpressionList parse(LexTokenReader ltr) throws LexException, ParserException
+	// {
+	// 	ltr.nextToken();
+	// 	ASTExpressionList args = new ASTExpressionList();
+	// 	ExpressionReader er = new ExpressionReader(ltr);
+	// 	ASTExpression exp = er.readExpression();
 		
-		if (exp instanceof ASTEqualsExpression)		// Should parse as an equals expression
-		{
-			ASTEqualsExpression eqexp = (ASTEqualsExpression)exp;
+	// 	if (exp instanceof ASTEqualsExpression)		// Should parse as an equals expression
+	// 	{
+	// 		ASTEqualsExpression eqexp = (ASTEqualsExpression)exp;
 			
-			if (eqexp.left instanceof ASTVariableExpression)
-			{
-				args.add(eqexp.left);
-				args.add(eqexp.right);
-			}
-			else
-			{
-				//parseException("expecting <name> = <exp>;", eqexp.location);
-			}
-		}
-		else
-		{
-			//parseException("expecting <name> = <exp>;", exp.location);
-		}
+	// 		if (eqexp.left instanceof ASTVariableExpression)
+	// 		{
+	// 			args.add(eqexp.left);
+	// 			args.add(eqexp.right);
+	// 		}
+	// 		else
+	// 		{
+	// 			//parseException("expecting <name> = <exp>;", eqexp.location);
+	// 		}
+	// 	}
+	// 	else
+	// 	{
+	// 		//parseException("expecting <name> = <exp>;", exp.location);
+	// 	}
 		
-		if (ltr.getLast().isNot(Token.SEMICOLON))
-		{
-			//parseException("missing ;", ltr.getLast().location);
-		}
+	// 	if (ltr.getLast().isNot(Token.SEMICOLON))
+	// 	{
+	// 		//parseException("missing ;", ltr.getLast().location);
+	// 	}
 		
-		return args;
-	}
+	// 	return args;
+	// }
 }
