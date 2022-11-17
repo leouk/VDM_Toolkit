efbb1c2cbb4e191e877fef56b61cd6ca50a2bd43
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2022-11-17 11:13:35 +0000

    setlinks doesn't work for HP.  Or else, have to branch / build on HP then switch back etc. Leave for now.

--- scripts/setlinks.sh
+++ scripts/setlinks.sh
@@ -26,7 +26,7 @@ VDMTOOLKIT="vdmtoolkit"
 SNAPSHOT="-SNAPSHOT"
 # SNAPSHOT=""
 
-for DIR in jars/vdmj jars/vdmj_hp jars/plugins
+for DIR in jars/vdmj jars/plugins #jars/vdmj_hp 
 do
     rm $RESOURCES/$DIR/vdmj*.jar
     rm $RESOURCES/$DIR/lsp*.jar
@@ -39,19 +39,19 @@ do
 done
 
 ln -sf $HOME/.m2/repository/$GROUPID/annotations/$VDMJSUITE-SNAPSHOT/annotations-$VDMJSUITE-SNAPSHOT.jar $RESOURCES/jars/vdmj 	#$RESOURCES/jars/vdmj/annotations-$VDMJSUITE-SNAPSHOT.jar
-echo "Created annotations from $HOME/.m2/repository/$GROUPID/annotations/$VDMJSUITE-SNAPSHOT/annotations-$VDMJSUITE-SNAPSHOT.jar to $RESOURCES/jars/vdmj/annotations-$VDMJSUITE-SNAPSHOT.jar"
+#echo "Created annotations from $HOME/.m2/repository/$GROUPID/annotations/$VDMJSUITE-SNAPSHOT/annotations-$VDMJSUITE-SNAPSHOT.jar to $RESOURCES/jars/vdmj/annotations-$VDMJSUITE-SNAPSHOT.jar"
 ln -sf $HOME/.m2/repository/$GROUPID/annotations2/$VDMJSUITE-SNAPSHOT/annotations2-$VDMJSUITE-SNAPSHOT.jar $RESOURCES/jars/vdmj # $RESOURCES/jars/vdmj/annotations2-$VDMJSUITE-SNAPSHOT.jar
 ln -sf $HOME/.m2/repository/$GROUPID/vdmj/$VDMJSUITE-SNAPSHOT/vdmj-$VDMJSUITE-SNAPSHOT.jar $RESOURCES/jars/vdmj 				#$RESOURCES/jars/vdmj/vdmj-$VDMJSUITE-SNAPSHOT.jar
 ln -sf $HOME/.m2/repository/$GROUPID/lsp/$VDMJSUITE-SNAPSHOT/lsp-$VDMJSUITE-SNAPSHOT.jar $RESOURCES/jars/vdmj 					#$RESOURCES/jars/vdmj/lsp-$VDMJSUITE-SNAPSHOT.jar
 ln -sf $HOME/.m2/repository/$GROUPID/stdlib/$VDMJSUITE-SNAPSHOT/stdlib-$VDMJSUITE-SNAPSHOT.jar $RESOURCES/jars/vdmj/libs 		#$RESOURCES/jars/vdmj/libs/stdlib-$VDMJSUITE-SNAPSHOT.jar
-echo "Created jars/vdmj links"
+#echo "Created jars/vdmj links"
 
-ln -sf $HOME/.m2/repository/$GROUPID/annotations/$VDMJSUITE-P-SNAPSHOT/annotations-$VDMJSUITE-P-SNAPSHOT.jar $RESOURCES/jars/vdmj_hp
-ln -sf $HOME/.m2/repository/$GROUPID/annotations2/$VDMJSUITE-P-SNAPSHOT/annotations2-$VDMJSUITE-P-SNAPSHOT.jar $RESOURCES/jars/vdmj_hp
-ln -sf $HOME/.m2/repository/$GROUPID/vdmj/$VDMJSUITE-P-SNAPSHOT/vdmj-$VDMJSUITE-P-SNAPSHOT.jar $RESOURCES/jars/vdmj_hp
-ln -sf $HOME/.m2/repository/$GROUPID/lsp/$VDMJSUITE-P-SNAPSHOT/lsp-$VDMJSUITE-P-SNAPSHOT.jar $RESOURCES/jars/vdmj_hp
-ln -sf $HOME/.m2/repository/$GROUPID/stdlib/$VDMJSUITE-P-SNAPSHOT/stdlib-$VDMJSUITE-P-SNAPSHOT.jar $RESOURCES/jars/vdmj_hp/libs
-echo "Created jars/vdmj_hp links"
+#ln -sf $HOME/.m2/repository/$GROUPID/annotations/$VDMJSUITE-P-SNAPSHOT/annotations-$VDMJSUITE-P-SNAPSHOT.jar $RESOURCES/jars/vdmj_hp
+#ln -sf $HOME/.m2/repository/$GROUPID/annotations2/$VDMJSUITE-P-SNAPSHOT/annotations2-$VDMJSUITE-P-SNAPSHOT.jar $RESOURCES/jars/vdmj_hp
+#ln -sf $HOME/.m2/repository/$GROUPID/vdmj/$VDMJSUITE-P-SNAPSHOT/vdmj-$VDMJSUITE-P-SNAPSHOT.jar $RESOURCES/jars/vdmj_hp
+#ln -sf $HOME/.m2/repository/$GROUPID/lsp/$VDMJSUITE-P-SNAPSHOT/lsp-$VDMJSUITE-P-SNAPSHOT.jar $RESOURCES/jars/vdmj_hp
+#ln -sf $HOME/.m2/repository/$GROUPID/stdlib/$VDMJSUITE-P-SNAPSHOT/stdlib-$VDMJSUITE-P-SNAPSHOT.jar $RESOURCES/jars/vdmj_hp/libs
+#echo "Created jars/vdmj_hp links"
 
 ln -sf $HOME/.m2/repository/$VDMTOOLKIT/annotationsVDMToolkit/$VDMTKSUITE-SNAPSHOT/annotationsVDMToolkit-$VDMTKSUITE-SNAPSHOT.jar $RESOURCES/jars/vdmj
 ln -sf $HOME/.m2/repository/$VDMTOOLKIT/vdm2isa/$VDMTKSUITE-SNAPSHOT/vdm2isa-$VDMTKSUITE-SNAPSHOT.jar $RESOURCES/jars/plugins
