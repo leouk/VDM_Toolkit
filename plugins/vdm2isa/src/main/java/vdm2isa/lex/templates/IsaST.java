5b5943348d43d1c9993dede1e2d805f03f011195
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2022-12-01 10:24:31 +0000

    started playing with StringTemplate4

---
+++ plugins/vdm2isa/src/main/java/vdm2isa/lex/templates/IsaST.java
@@ -0,0 +1,38 @@
+package vdm2isa.lex.templates;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+
+import org.stringtemplate.v4.ST;
+import org.stringtemplate.v4.STGroup;
+import org.stringtemplate.v4.STGroupFile;
+
+public class IsaST {
+    
+    public static final String ISA_TEMPLATE_GROUPDIR = "src/main/resources/templates/";
+
+    public static void main(String[] args) throws IOException
+    {
+        STGroup.trackCreationEvents = true;
+        STGroup group = new STGroupFile(ISA_TEMPLATE_GROUPDIR + "theory.stg", '$', '$');
+        ST st = group.getInstanceOf("theory");
+        st.add("utc", "AGORA!");
+        st.add("location", "LOCATION!");
+        st.add("name", "Test");
+        st.add("imports", "OtherFile");
+        st.add("body", "\tnothing");
+        System.out.println(st.render());
+
+        st = group.getInstanceOf("abbreviation");
+        st.add("name", "test");
+        st.add("type", "VDMNat \\<Rightarrow> \\<bool>");
+        st.add("attr", "simp");
+        st.add("attr", "intro!");
+        st.add("expr", "True");
+        System.out.println(st.render());
+
+        st.inspect();
+    }  
+}
