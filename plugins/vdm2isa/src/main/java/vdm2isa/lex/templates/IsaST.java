aaf561b4112946fab7c8fce61ec957adb293afbb
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2022-12-01 11:11:20 +0000

    ST working nicely. Try JavaBeaming it.

--- plugins/vdm2isa/src/main/java/vdm2isa/lex/templates/IsaST.java
+++ plugins/vdm2isa/src/main/java/vdm2isa/lex/templates/IsaST.java
@@ -13,6 +13,12 @@ public class IsaST {
     
     public static final String ISA_TEMPLATE_GROUPDIR = "src/main/resources/templates/";
 
+    public static class IsaTheory {
+        public static final String TEMPLATE = "theory";
+
+
+    } 
+
     public static void main(String[] args) throws IOException
     {
         STGroup.trackCreationEvents = true;
@@ -28,6 +34,40 @@ public class IsaST {
         st = group.getInstanceOf("abbreviation");
         st.add("name", "test");
         st.add("type", "VDMNat \\<Rightarrow> \\<bool>");
+        st.add("expr", "True");
+        System.out.println(st.render());
+
+        st = group.getInstanceOf("definition");
+        st.add("name", "test");
+        st.add("type", "VDMNat \\<Rightarrow> \\<bool>");
+        st.add("attr", "simp");
+        st.add("attr", "intro!");
+        st.add("expr", "True");
+        System.out.println(st.render());
+
+        st = group.getInstanceOf("tsynonym");
+        st.add("name", "'a");
+        st.add("name", "test");
+        st.add("expr", "'a set");
+        System.out.println(st.render());
+
+        st = group.getInstanceOf("lemmas");
+        st.add("name", "test");
+        st.add("lemma", "f_def");
+        st.add("lemma", "g_def");
+        System.out.println(st.render());
+
+        st = group.getInstanceOf("claim");
+        st.add("kind", "lemma");
+        st.add("name", "test");
+        st.add("attr", "simp");
+        st.add("attr", "intro!");
+        st.add("expr", "True");
+        System.out.println(st.render());
+
+        st = group.getInstanceOf("claim");
+        st.add("kind", "theorem");
+        st.add("name", "test");
         st.add("attr", "simp");
         st.add("attr", "intro!");
         st.add("expr", "True");
