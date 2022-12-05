792c3902a2cb7f325278ca85e6a031bee6c73c18
Leo Freitas <leo.freitas@newcastle.ac.uk>
2022-12-05 15:32:51 +0000

    statements now also in separate packages

---
+++ plugins/vdm2isa/src/main/java/vdm2isa/tr/statements/TRStateDesignator.java
@@ -0,0 +1,29 @@
+package vdm2isa.tr.statements;
+
+import com.fujitsu.vdmj.lex.LexLocation;
+
+import vdm2isa.lex.IsaToken;
+import vdm2isa.tr.expressions.TRExpression;
+import vdm2isa.tr.expressions.visitors.TRExpressionVisitor;
+
+// @JK
+// This is built on a hack as it technically needs its own base since it seems to have no connection to Expression
+public abstract class TRStateDesignator extends TRExpression{
+
+    public TRStateDesignator(LexLocation location)
+	{
+		super(location, null, null);
+	}
+
+    public <R, S> R apply(TRExpressionVisitor<R, S> visitor, S arg) 
+    {
+        return visitor.caseTRStateDesignator(this, arg);
+    }
+
+    public abstract String translate();
+
+    public IsaToken isaToken() {
+        return IsaToken.EOF;
+    }
+    
+}
---
+++ plugins/vdm2isa/src/main/java/vdm2isa/tr/statements/TRStatement.java
@@ -0,0 +1,53 @@
+package vdm2isa.tr.statements;
+
+import com.fujitsu.vdmj.lex.LexLocation;
+import com.fujitsu.vdmj.tc.statements.TCStatement;
+
+import vdm2isa.lex.IsaToken;
+import vdm2isa.lex.TRIsaVDMCommentList;
+import vdm2isa.tr.expressions.TRExpression;
+import vdm2isa.tr.expressions.visitors.TRExpressionVisitor;
+import vdm2isa.tr.types.TRType;
+
+// @JK
+// Is it ok that we are fudging it into an expression?
+public abstract class TRStatement extends TRExpression {
+
+    TRIsaVDMCommentList comments;
+
+    TCStatement expS;
+
+    public TRStatement(LexLocation location, TRIsaVDMCommentList comments, TCStatement exp, TRType stmttype)
+	{
+		super(location, null, stmttype);
+        this.expS = exp;
+        this.comments = comments;
+	}
+
+    public <R, S> R apply(TRExpressionVisitor<R, S> visitor, S arg) 
+    {
+        return visitor.caseStatement(this, arg);
+    }
+
+    public String translate(){
+        return comments.translate();// + TRStateDefinition.state.recordType.getInvDef().getParameters().patternContextTranslate(null) + "\n\t";
+    }
+
+    public IsaToken isaToken() {
+        return IsaToken.EOF;
+    }
+
+    
+    // public TRExpression(LexLocation location, TCExpression exp, TRType exptype)
+	// {
+	// 	super(location);
+    //     this.exp = exp;
+    //     this.exptype = exptype;
+    //     this.hasWarnedAboutUnknownType = false;
+    //     this.hasWarnedAboutNullType = false;
+    //     this.exprFVV = new TCFreeVariableExpressionVisitor(new TCGetFreeVariablesVisitorSet());
+    //     //this.fvvENV = new EnvTriple(new FlatEnvironment(new TCDefinitionList()), new FlatEnvironment(new TCDefinitionList()), new AtomicBoolean(false)); 
+    //     this.fvvENV = new FlatEnvironment(new TCDefinitionList());
+	// }
+
+}
