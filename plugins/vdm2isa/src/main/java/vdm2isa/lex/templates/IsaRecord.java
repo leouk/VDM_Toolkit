d666ebdf0db7eb2c210cd2ebd5669412b67b421a
Leo Freitas <leo.freitas@newcastle.ac.uk>
2022-12-05 14:29:06 +0000

    spliced view of record. don't like it but might need if template choice fails.

--- plugins/vdm2isa/src/main/java/vdm2isa/lex/templates/IsaRecord.java
+++ plugins/vdm2isa/src/main/java/vdm2isa/lex/templates/IsaRecord.java
@@ -1,14 +1,35 @@
 package vdm2isa.lex.templates;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class IsaRecord extends IsaNamedTemplate {
 
-    public final List<IsaRecordField> field; 
+    public final List<IsaRecordField> fields; 
 
-    public IsaRecord(IsaIdentifier name, IsaRecordField... field)
+    public IsaRecord(IsaIdentifier name, IsaRecordField... fields)
     {
         super(name); 
-        this.field = IsaAbstractTemplate.createList(field);
+        this.fields = IsaAbstractTemplate.createList(fields);
+    }
+
+    public final List<String> getFieldName()
+    {
+        List<String> result = new ArrayList<String>();
+        for(IsaRecordField f : fields)
+        {
+            result.add(f.name.toString());
+        }
+        return result;
+    }
+
+    public final List<String> getFieldType()
+    {
+        List<String> result = new ArrayList<String>();
+        for(IsaRecordField f : fields)
+        {
+            result.add(f.type.toString());
+        }
+        return result;
     }
 }
