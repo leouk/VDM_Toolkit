e8ef432a435e60f2520f2326555a8d7dce59315a
Leo Freitas <leo.freitas@newcastle.ac.uk>
2022-12-08 10:43:54 +0000

    add proof to claim

--- plugins/vdm2isa/src/main/java/vdm2isa/tr/templates/IsaClaim.java
+++ plugins/vdm2isa/src/main/java/vdm2isa/tr/templates/IsaClaim.java
@@ -10,12 +10,14 @@ public class IsaClaim extends IsaNamedTemplate {
     public final ClaimKind kind;
     public final List<IsaAttribute> attr;
     public final String expr; 
+    public final List<String> proof;
 
-    protected IsaClaim(String comment, ClaimKind kind, IsaIdentifier name, List<IsaAttribute> attr, String expr)
+    protected IsaClaim(String comment, ClaimKind kind, IsaIdentifier name, List<IsaAttribute> attr, String expr, List<String> proof)
     {
         super(comment, Arrays.asList(name));
         this.kind = kind;
         this.attr = IsaAbstractTemplate.createList(attr); 
         this.expr = expr; 
+        this.proof = IsaAbstractTemplate.createList(proof);
     }
 }
