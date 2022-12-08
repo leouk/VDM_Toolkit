169126a70f0b687e1a6796eb528df867a83f0890
Leo Freitas <leo.freitas@newcastle.ac.uk>
2022-12-08 10:07:51 +0000

    empty example for general isa plugin default (instead of VDMToolkit.vdmsl)!

---
+++ plugins/vdm2isa/src/test/resources/.generated/TestV2IEmpty.thy
@@ -0,0 +1,13 @@
+(* VDM to Isabelle Translation @2022-12-08T09:25:23.880Z  
+   Copyright 2019-22, Leo Freitas, leo.freitas@newcastle.ac.uk
+
+   VDM translation of module TestV2IEmpty
+   in 'TestV2IEmpty.vdmsl' at line 1:8
+   files = [TestV2IEmpty.vdmsl]
+*)
+theory TestV2IEmpty
+imports VDMToolkit 
+begin
+
+ 
+end 
