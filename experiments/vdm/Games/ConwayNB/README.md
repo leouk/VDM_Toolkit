f1588d561d328af367ed8855b92e6cdd5d471dee
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2023-01-30 11:33:45 +0000

    made it Maven dependency so that it will download VDMJ if/as needed ;-).

--- experiments/vdm/Games/ConwayNB/README.md
+++ experiments/vdm/Games/ConwayNB/README.md
@@ -16,3 +16,5 @@ Meanwhile, the compiled output files will be generated in the `bin` folder by de
 ## Dependency Management
 
 The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+
+You need to have VDMJ and ConwayNB installed on maven
--- experiments/vdm/Games/ConwayNB/conway_run.sh
+++ experiments/vdm/Games/ConwayNB/conway_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rlwrap java -cp ~/.m2/repository/dk/au/ece/vdmj/vdmj/4.5.0-SNAPSHOT/vdmj-4.5.0-SNAPSHOT.jar:bin com.fujitsu.vdmj.VDMJ -vdmsl -remote Conway Conway.vdmsl
+rlwrap java -cp ~/.m2/repository/dk/au/ece/vdmj/vdmj/4.5.0-SNAPSHOT/vdmj-4.5.0-SNAPSHOT.jar:~/.m2/repository/vdmtoolkit/ConwayNB/1.0-SNAPSHOT:bin com.fujitsu.vdmj.VDMJ -vdmsl -remote Conway Conway.vdmsl
