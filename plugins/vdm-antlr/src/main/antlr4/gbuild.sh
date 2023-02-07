5186200b5c1b2bad924962758bfa8fbdfbe8a9cb
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2023-02-07 12:47:07 +0000

    allow for separate test + build on cmd line as vscode-antlr4 visualisation ca be a bit funny for larger files.

--- plugins/vdm-antlr/src/main/antlr4/gbuild.sh
+++ plugins/vdm-antlr/src/main/antlr4/gbuild.sh
@@ -2,7 +2,7 @@
 
 # delete previous builds
 echo 'Removing out generated grammars'
-rm -R ./.antlr
+rm -R ./.antlr/output
 
 	# alias antlr4='java -Xmx500M -cp /usr/local/lib/antlr4-4.11.1-complete.jar:$CLASSPATH org.antlr.v4.Tool' 
 	# alias grun='java -Xmx500M -cp /usr/local/lib/antlr4-4.11.1-complete.jar:$CLASSPATH org.antlr.v4.gui.TestRig'
@@ -15,17 +15,21 @@ export JAVA_HOME=$(/usr/libexec/java_home -v11)
 # presumes ANTLR is somewhere sensible 
 #TODO generalise to find in Maven Repo or download or etc. 
 
+ANTLR4=/usr/local/lib/antlr4-4.11.1-complete.jar
+VDMJ=~/.m2/repository/dk/au/ece/vdmj/vdmj/4.5.0-SNAPSHOT/vdmj-4.5.0-SNAPSHOT.jar
+CP="$ANTLR4:$VDMJ:$CLASSPATH"
+
 # call ANTLR to generate parser
-# antlr4 VDM.g4 VDMLex.g4 -listener -visitor -Xlog -atn -o ./.antlr
-echo 'Calling ANTLR4 parser generator'
+# antlr4 VDM.g4 VDMLex.g4 -listener -visitor -Xlog -atn -o ./.antlr/output
+echo 'Calling ANTLR4 parser generator' #' with CP ' $CP
 # -package vdmantlr
-java -Xmx500M -cp /usr/local/lib/antlr4-4.11.1-complete.jar:$CLASSPATH org.antlr.v4.Tool VDM.g4 VDMLex.g4 -listener -visitor -Xlog -atn -o ./.antlr
+java -Xmx500M -cp $CP org.antlr.v4.Tool VDM.g4 VDMLex.g4 -listener -visitor -atn -o ./.antlr/output
 
 # compile generated files
-echo 'Compiling generarted parser'
-javac ./.antlr/*.java
+#echo 'Compiling generarted parser'
+#javac ./.antlr/output/*.java
 
-source gtest.sh "$@"
+#source gtest.sh "$@"
 #TODO allow starting in inner rules beyond sl_document?
 #TODO grun has to be on the same directory as VDM.class from VDM.g4->VDM.java generated above, hence the "../$@" :-(
 #grun VDM sl_document -tree -gui "$@"
--- plugins/vdm-antlr/src/main/antlr4/gtest.sh
+++ plugins/vdm-antlr/src/main/antlr4/gtest.sh
@@ -9,26 +9,30 @@ export JAVA_HOME=$(/usr/libexec/java_home -v11)
 #TODO grun has to be on the same directory as VDM.class from VDM.g4->VDM.java generated above, hence the "../$@" :-(
 #grun VDM sl_document -tree -gui "$@"
 echo 'Calling ANTLR grun test rig'
-cd .antlr
+cd .antlr/output
 
 # compile generated files
 echo 'Compiling generarted parser'
 javac *.java
 
+ANTLR4=/usr/local/lib/antlr4-4.11.1-complete.jar
+VDMJ=~/.m2/repository/dk/au/ece/vdmj/vdmj/4.5.0-SNAPSHOT/vdmj-4.5.0-SNAPSHOT.jar
+CP="$ANTLR4:$VDMJ:$CLASSPATH"
+
 START_PRODUCTION=sl_document
 if [[ "$#" -gt 1 ]]; then
     START_PRODUCTION=$1
     # gets the second arguments onwards
-    ARGS=../"${@:2}"   
+    ARGS=../../"${@:2}"   
 else 
     # gets all arguments
-    ARGS=../"${@}"
+    ARGS=../../"${@}"
 fi
-echo 'Parsing from ' $START_PRODUCTION ' for file ' $ARGS
-java -Xmx500M -cp /usr/local/lib/antlr4-4.11.1-complete.jar:$CLASSPATH org.antlr.v4.gui.TestRig VDM $START_PRODUCTION -tokens -gui $ARGS
+echo 'Parsing from ' $START_PRODUCTION ' for file ' $ARGS ' with CP ' $CP
+java -Xmx500M -cp $CP org.antlr.v4.gui.TestRig VDM $START_PRODUCTION -tokens -gui $ARGS
 
 echo 'Restoring default java version'
-cd ..
+cd ../..
 export JAVA_HOME=$OLD_JAVA_HOME
 
 echo 'Done'
