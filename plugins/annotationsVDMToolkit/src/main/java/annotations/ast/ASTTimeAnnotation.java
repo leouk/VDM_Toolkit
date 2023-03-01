3b023614dd41c2d664d76acb6bab53500337f7c8
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2023-03-01 12:00:34 +0000

    toolkit annotations update

--- plugins/annotationsVDMToolkit/src/main/java/annotations/ast/ASTTimeAnnotation.java
+++ plugins/annotationsVDMToolkit/src/main/java/annotations/ast/ASTTimeAnnotation.java
@@ -7,6 +7,9 @@ package annotations.ast;
 import com.fujitsu.vdmj.ast.annotations.ASTAnnotation;
 import com.fujitsu.vdmj.ast.lex.LexIdentifierToken;
 
+/**
+ * Annotation must be called @Time
+ */
 public class ASTTimeAnnotation extends ASTAnnotation
 {
 	private static final long serialVersionUID = 1L;
