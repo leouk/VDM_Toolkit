a277cfb495ea7ce5c0843d4f278c7bae2e7e961c
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2023-03-02 13:34:51 +0000

    attempt to create a LexLocation aware Range. Not sure how this will go.

--- plugins/vdm-antlr/src/main/java/vdmantlr/VDMParserUtils.java
+++ plugins/vdm-antlr/src/main/java/vdmantlr/VDMParserUtils.java
@@ -1,8 +1,8 @@
 package vdmantlr;
 
+
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.RuleContext;
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.misc.Interval;
 
@@ -79,8 +79,28 @@ public class VDMParserUtils {
         }
     }
 
+    public static Range createRange(String identifier, ParserRuleContext ctx, ParserRuleContext parentCandidate)
+    {
+        // scope goes from the end of the declaration to the end of the parent node
+        Token start = ctx.getStop();
+        Token stop = parentCandidate.getStop();
+        int start_line = start.getLine();
+        int from_col = start.getCharPositionInLine() + 1 + start.getText().length();
+        int stop_line = stop.getLine();
+        int to_col = stop.getCharPositionInLine() + 1 + stop.getText().length();
+        System.out.println("\nSCOPE of " + identifier + " is " + ((parentCandidate instanceof VDMParser.Sl_documentContext) ? "whole module" : getSource(parentCandidate)));
+        System.out.println("start " + start_line + "(" + from_col + ")");
+        System.out.println("stop " + stop_line + "(" + to_col + ")");
+        return new Range(start_line, from_col, stop_line, to_col);
+    }
+
+    public static Range findScope(ParserRuleContext ctx)
+    {
+        return findScope(null, ctx);
+    }
+
     /**
-     * Find the scope of an identifier
+     * Find the scope of an identifier. Identifier can be null for contexts not involving names
      * @param identifier (String) Name of the identifier
      * @param ctx (ParserRuleContext) ANTLR context of the identifier
      */
@@ -89,55 +109,53 @@ public class VDMParserUtils {
         ParserRuleContext candidate = ctx.getParent();
         while (candidate != null) 
         {
-            if (candidate instanceof VDMParser.Sl_type_definitionsContext
-                || candidate instanceof VDMParser.State_definitionContext
-                || candidate instanceof VDMParser.Sl_value_definitionsContext
-                || candidate instanceof VDMParser.Sl_function_definitionsContext
-                || candidate instanceof VDMParser.Sl_operation_definitionsContext
-                || candidate instanceof VDMParser.Traces_definitionsContext) 
+            // how wide to go depends on who needs the location information? 
+            if (   candidate instanceof VDMParser.PatternContext
+                || candidate instanceof VDMParser.BindContext
+                || candidate instanceof VDMParser.ExpressionContext
+                || candidate instanceof VDMParser.TypeContext
+                || candidate instanceof VDMParser.StatementContext
+                || candidate instanceof VDMParser.NameContext
+                // || candidate instanceof VDMParser.State_definitionContext
+                // || candidate instanceof VDMParser.Sl_value_definitionsContext
+                // || candidate instanceof VDMParser.Sl_function_definitionsContext
+                // || candidate instanceof VDMParser.Sl_operation_definitionsContext
+                // || candidate instanceof VDMParser.Traces_definitionsContext
+               ) 
             {
-                // scope goes from the end of the declaration to the end of the parent node
-                Token start = ctx.getStop();
-                Token stop = candidate.getStop();
-                int start_line = start.getLine();
-                int from_col = start.getCharPositionInLine() + 1 + start.getText().length();
-                int stop_line = stop.getLine();
-                int to_col = stop.getCharPositionInLine() + 1 + stop.getText().length();
-                // System.out.println("\nSCOPE of " + identifier + " is " + ((candidate instanceof PvsLanguageParser.TheoryContext) ? "THEORY" : getSource(candidate)));
-                // System.out.println("start " + start_line + "(" + from_col + ")");
-                // System.out.println("stop " + stop_line + "(" + to_col + ")");
-                return new Range(start_line, from_col, stop_line, to_col);
+                return createRange(identifier, ctx, candidate);
             }
             candidate = candidate.getParent();
         }
         return null;
     }
-    public static String findScopeName (ParserRuleContext ctx) {
-        String scopeName = null;
-        RuleContext candidate = ctx.parent;
-        while (candidate != null) 
-        {
-            if (candidate instanceof VDMParser.Type_definitionContext) 
-            {
-                VDMParser.Type_definitionContext c = (VDMParser.Type_definitionContext) candidate;
-                scopeName = c.IDENTIFIER().getText();
-                break;
-            }
-            if (candidate instanceof VDMParser.Function_definitionContext) 
-            {
-                VDMParser.Function_definitionContext c = (VDMParser.Function_definitionContext) candidate;
-                if (c.explicit_function_definition() != null)
-                    scopeName = c.explicit_function_definition().IDENTIFIER(0).getText();
-                else if (c.implicit_function_definition() != null)
-                    scopeName = c.implicit_function_definition().IDENTIFIER().getText();
-                else if (c.extended_explicit_function_definition() != null)
-                    scopeName = c.extended_explicit_function_definition().IDENTIFIER().getText();
-                //break;
-            }
-            candidate = candidate.parent;
-        }
-        return scopeName;
-    }
+
+    // public static String findScopeName (ParserRuleContext ctx) {
+    //     String scopeName = null;
+    //     RuleContext candidate = ctx.parent;
+    //     while (candidate != null) 
+    //     {
+    //         if (candidate instanceof VDMParser.Type_definitionContext) 
+    //         {
+    //             VDMParser.Type_definitionContext c = (VDMParser.Type_definitionContext) candidate;
+    //             scopeName = c.IDENTIFIER().getText();
+    //             break;
+    //         }
+    //         if (candidate instanceof VDMParser.Function_definitionContext) 
+    //         {
+    //             VDMParser.Function_definitionContext c = (VDMParser.Function_definitionContext) candidate;
+    //             if (c.explicit_function_definition() != null)
+    //                 scopeName = c.explicit_function_definition().IDENTIFIER(0).getText();
+    //             else if (c.implicit_function_definition() != null)
+    //                 scopeName = c.implicit_function_definition().IDENTIFIER().getText();
+    //             else if (c.extended_explicit_function_definition() != null)
+    //                 scopeName = c.extended_explicit_function_definition().IDENTIFIER().getText();
+    //             //break;
+    //         }
+    //         candidate = candidate.parent;
+    //     }
+    //     return scopeName;
+    // }
 
     /**
      * Returns the fragment of source code associated to a given context
