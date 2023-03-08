b58360dc68a29effff2b24c68e277b1e549b361f
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2023-03-08 10:24:09 +0000

    sets example

--- plugins/vdm2isa/pub/recursion/RecursiveVDMExamples.thy
+++ plugins/vdm2isa/pub/recursion/RecursiveVDMExamples.thy
@@ -49,13 +49,14 @@ function (domintros) factZ :: \<open>VDMNat \<Rightarrow> VDMNat\<close> where
 (********************************************************)
 section \<open>Simple recursion catering for VDM specification\<close>
 
+\<comment> \<open>Automatically generated: implicitly inferred input type invariant check\<close>
 definition pre_factV :: \<open>VDMNat \<Rightarrow> \<bool>\<close> where 
 \<open>pre_factV n \<equiv> inv_VDMNat n\<close>
 
 \<comment> \<open>VDM only operates if precondition is satisfied\<close>
 function (domintros) factV :: \<open>VDMNat \<Rightarrow> VDMNat\<close> where
 \<open>factV n = (if pre_factV n then (if n = 0 then 1 else n * (factV (n - 1))) else undefined)\<close>
-  by (pat_completeness, auto) 
+  by (pat_completeness, auto) \<^marker>\<open>tag sledgehammer\<close>
 
   \<comment> \<open>Well formedness expression relating conditions for every recursive and original calls\<close>
   abbreviation factV_wf :: \<open>(VDMNat \<times> VDMNat) set\<close> where
@@ -68,14 +69,14 @@ function (domintros) factV :: \<open>VDMNat \<Rightarrow> VDMNat\<close> where
   termination
     apply (relation \<open>(gen_VDMNat_term factV_wf)\<close>) 
     \<comment> \<open>This enables sledgehammer to find the well-formedness part of the proof\<close>
-    using l_gen_VDMNat_term_wf apply blast
+    using l_gen_VDMNat_term_wf apply blast \<^marker>\<open>tag sledgehammer\<close>
     \<comment> \<open>Remains to be shown that the local recursive relation is within the general relation space\<close>
     (*Sledgehammering... No proof found *)
     oops
 
   termination 
     apply (relation \<open>(gen_VDMNat_term factV_wf)\<close>) 
-    using l_gen_VDMNat_term_wf apply blast
+    using l_gen_VDMNat_term_wf apply blast \<^marker>\<open>tag sledgehammer\<close>
     \<comment> \<open>Even though sledgehammer struggles, the proof is in fact trivial\<close>
     by (simp add: pre_factV_def int_ge_less_than_def)
 
@@ -92,6 +93,55 @@ lemma l_fact_term_valid: \<open>(gen_VDMNat_term factV_wf) = factV_wf\<close>
   apply (intro equalityI subsetI)
   apply (simp_all add: pre_factV_def int_ge_less_than_def case_prod_beta)
   by auto
+
+section \<open>VDM recursion over sets\<close>
+
+\<comment> \<open>  
+  sumset: set of nat -> nat 
+  sumset(s) == if s = {} then 0 else let e in set s in sumset(s - {e}) + e
+  pre (forall n in set s & n > 5)
+  --@IsaMeasure({(x - { let e in set x in e }, x) | x : set of nat & x <> {}}) 
+  --@Witness(sumset({ 1 }))
+  measure card s;
+\<close>
+
+\<comment> \<open>Automatically generated: implicitly inferred type invariant check + user defined pre\<close>
+definition pre_sumset :: \<open>VDMNat VDMSet \<Rightarrow> \<bool>\<close> where
+  \<open>pre_sumset s \<equiv> inv_VDMSet' inv_VDMNat s \<and> (\<forall> n \<in> s . n > 5)\<close>
+
+\<comment> \<open>Automatically generated def set: inferred from function AST + signature\<close>
+lemmas pre_sumset_defs = pre_sumset_def inv_VDMSet'_defs 
+
+\<comment> \<open>Mostly verbatim translation from VDM; let-in-set becomes Isabelle's Hilbert Choice binder (\<some>)\<close>
+function (domintros) sumset :: \<open>VDMNat VDMSet \<Rightarrow> VDMNat\<close> where 
+  \<open>sumset s = (if pre_sumset s then 
+                  (if s = {} then 0 else 
+                      let e = (\<some> x . x \<in> s) in sumset (s - {e}) + e) 
+                   else undefined)\<close>
+  \<comment> \<open>Automatically generated proof for pattern compatibility and completeness\<close>
+  \<comment> \<open>Users will have to finish this before proceeding if proof suggestion fails!\<close>
+  by (pat_completeness, auto)
+
+  \<comment> \<open>Recursive definitions available, yet as partial functions (psimps)\<close>
+  find_theorems name:"sumset"
+
+  \<comment> \<open>Well founded recursive relation translated from user defined @IsaMeasure\<close>
+  \<comment> \<open>We automatically infer recursive relations for this specific (commonly used) kind of set recursion\<close>
+  \<comment> \<open>It is crucial for termination proof that pre condition is included, which translator does automatically\<close>
+  abbreviation sumset_wf_rel :: \<open>(VDMNat VDMSet \<times> VDMNat VDMSet) set\<close> where
+    \<open>sumset_wf_rel \<equiv> { (s - {(\<some> e . e \<in> s)}, s)| s . pre_sumset s \<and> s \<noteq> {}}\<close>
+
+  \<comment> \<open>Translator infers recursive relation well formedness lemma being about sets\<close>
+  lemma l_sumset_rel_wf: \<open>wf (gen_set_term sumset_wf_rel)\<close>
+    \<comment> \<open>Proof in this case is discovered by sledgehammer\<close>
+    using l_gen_set_term_wf by blast \<^marker>\<open>tag sledgehammer\<close>
+
+  \<comment> \<open>Termination proof setup is automatically generated\<close>
+  termination
+    apply (relation \<open>(gen_set_term sumset_wf_rel)\<close>)
+    using l_sumset_rel_wf apply blast \<^marker>\<open>tag sledgehammer\<close>
+    oops
+
 
 end
 (*>*)
