fc0b9276f94a1597ddc22689fad8ed20b718636f
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2023-03-18 15:47:53 +0000

    add few extra flags, including one that parses with VDMJ's parser too ;-).

--- plugins/vdm-antlr/src/main/java/vdmantlr/ANTLRASTPlugin.java
+++ plugins/vdm-antlr/src/main/java/vdmantlr/ANTLRASTPlugin.java
@@ -9,20 +9,29 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 import com.fujitsu.vdmj.Settings;
+import com.fujitsu.vdmj.ast.modules.ASTModuleList;
 import com.fujitsu.vdmj.lex.Dialect;
+import com.fujitsu.vdmj.lex.LexTokenReader;
 import com.fujitsu.vdmj.messages.InternalException;
 import com.fujitsu.vdmj.messages.VDMMessage;
 import com.fujitsu.vdmj.plugins.PluginConsole;
 import com.fujitsu.vdmj.plugins.VDMJ;
 import com.fujitsu.vdmj.plugins.analyses.ASTPlugin;
 import com.fujitsu.vdmj.plugins.analyses.ASTPluginSL;
+import com.fujitsu.vdmj.syntax.ModuleReader;
 
 public class ANTLRASTPlugin extends ASTPluginSL
 {
 
     public static void main(String[] args)
     {
-        VDMJ.main(new String[] {"-vdmsl", "-strict", "-annotations", "-i", "-verbose", "-antlr", "sl_document" 
+        VDMJ.main(new String[] {"-vdmsl", "-strict", "-annotations", "-i" 
+            //, "-verbose" 
+            ,"-antlr", "sl_document" 
+            //, "-antlrAST"
+            , "-printAST" 
+            , "-std"
+            //, "-antlrDebug"
             , "./vdmslScenario/Types.vdmsl"
         });
     }
@@ -41,10 +50,18 @@ public class ANTLRASTPlugin extends ASTPluginSL
 
     public static String DEFAULT_RULE_NAME = "sl_document";
     private String ruleName;
+    private boolean antlrAST;
+    private boolean printAST;
+    private boolean stdParse;
+    private boolean antlrDebug;
 
     public ANTLRASTPlugin()
     {
         super();
+        antlrAST = false;
+        printAST = false;
+        stdParse = false;
+        antlrDebug = false;
         ruleName = DEFAULT_RULE_NAME;
     }
 
@@ -53,6 +70,10 @@ public class ANTLRASTPlugin extends ASTPluginSL
 	{
         super.usage();
 		PluginConsole.println("-antlr <rule-name>: select a specific ANTLR grammar entry point");
+		PluginConsole.println("-antlrAST: prints out ANTLR's S-expression of parse tree");
+		PluginConsole.println("-printAST: prints out VDM string of generated VDM AST");
+        PluginConsole.println("-antlrDebug: switches on various ANTLR diagnostics (slows parsing down)");
+        PluginConsole.println("-std: also parses and prints AST with VDMJ parser");
 	}
 
     private boolean validRuleName(String ruleName)
@@ -68,8 +89,7 @@ public class ANTLRASTPlugin extends ASTPluginSL
 		
 		while (iter.hasNext())
 		{
-            String p = iter.next();
-			switch (p)
+			switch (iter.next())
 			{
 				case "-antlr":
 	    			iter.remove();
@@ -90,6 +110,22 @@ public class ANTLRASTPlugin extends ASTPluginSL
 	    				PluginConsole.fail("-antlr option requires a valid ANTLR grammer rule name");
 	    			}
 	    			break;
+                case "-antlrAST":
+                    iter.remove();
+                    antlrAST = true;
+                    break;
+                case "-printAST":
+                    iter.remove();
+                    printAST = true;
+                    break;
+                case "-std":
+                    iter.remove();
+                    stdParse = true;
+                    break;
+                case "-antlrDebug":
+                    iter.remove();
+                    antlrDebug = true;
+                    break;
     		}
 		}
 	}
@@ -104,10 +140,26 @@ public class ANTLRASTPlugin extends ASTPluginSL
             VDMASTListener listener = null;			
 			try
 			{
-				listener = new VDMASTListener(file.getAbsolutePath(), Settings.filecharset);
+				listener = new VDMASTListener(file.getAbsolutePath(), Settings.filecharset, antlrDebug);
                 //TODO could permit VDM parsing from different places? Nice! 
                 ParseTree t = listener.production(ruleName);
                 ParseTreeWalker.DEFAULT.walk(listener, t);
+                String antlrVDMString = "switch \"-printAST\" first!";
+                if (antlrAST)
+                    PluginConsole.println("\ntree="+listener.toSString(t)+"\n");
+                if (printAST)
+                {
+                    antlrVDMString = listener.toVDMString(t);
+                    PluginConsole.println("VDM=" + antlrVDMString +"\n");
+                }
+                if (stdParse && ruleName.equals("sl_document"))
+                {
+                    ModuleReader mr = new ModuleReader(new LexTokenReader(file, Dialect.VDM_SL, Settings.filecharset));
+                    ASTModuleList list = mr.readModules();
+                    String stdString = list.toString();
+                    PluginConsole.println("StdVDM=" + stdString +"\n");
+                    PluginConsole.println("StdVDM equal ANTLRVDM? " + antlrVDMString.equals(stdString));
+                }
                 astModuleList.addAll(listener.getAST());
 			}
 			catch (InternalException e)
