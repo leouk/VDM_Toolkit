1f1ab469bac3dbea16fd41b1608bfbcd176c5459
Leo Freitas <Leo.Freitas@newcastle.ac.uk>
2024-01-03 09:43:55 +0000

    update QC name

--- plugins/quickcheck/src/main/java/quickcheck/strategies/ExpandQCStrategy.java
+++ plugins/quickcheck/src/main/java/quickcheck/strategies/ExpandQCStrategy.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.fujitsu.vdmj.in.expressions.INExpression;
-import com.fujitsu.vdmj.in.patterns.INBindingSetter;
+import com.fujitsu.vdmj.in.patterns.INBindingOverride;
 import com.fujitsu.vdmj.plugins.VDMJ;
 import com.fujitsu.vdmj.pog.ProofObligation;
 import com.fujitsu.vdmj.runtime.Context;
@@ -76,12 +76,12 @@ public class ExpandQCStrategy extends QCStrategy
 	}
 
 	@Override
-	public StrategyResults getValues(ProofObligation po, INExpression exp, List<INBindingSetter> binds, Context ctxt)
+	public StrategyResults getValues(ProofObligation po, INExpression exp, List<INBindingOverride> binds, Context ctxt)
 	{
 		Map<String, ValueList> values = new HashMap<String, ValueList>();
 		long before = System.currentTimeMillis();
 		
-		for (INBindingSetter bind: binds)
+		for (INBindingOverride bind: binds)
 		{
 			values.put(bind.toString(), new ValueList());	// ie. nothing, for every bind
 		}
